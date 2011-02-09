0ccbfb907d5fa855e38e3bc86191aeb539ee7b77
xiaozhewang <xiaozhe.wang@spreadtrum.com>
2011-02-09 10:37:13 +0800

    Modify the funciton that the sensor call the I2C interface.
    Change-Id: I8b7465ca0e7b4231eebaa8c927c6168ec49d5c53

---
+++ include/linux/dcam_sensor.h
@@ -0,0 +1,14 @@
+
+/*
+ * Definitions for dcam sensor.
+ */
+#ifndef _DCAM_SENSOR_H_
+#define _DCAM_SENSOR_H_
+
+#define SENSOR_MAIN_I2C_NAME "sensor_main"
+#define SENSOR_SUB_I2C_NAME "sensor_sub"
+#define SENSOR_MAIN_I2C_ADDR 0x30 //OV2655
+#define SENSOR_SUB_I2C_ADDR 0x21 //OV7690
+
+#endif /* _DCAM_SENSOR_H_ */
+
