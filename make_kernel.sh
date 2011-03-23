42d98f99ccb7a393da920bc0ca55aec56b7251f4
gelixin <luther.ge@spreadtrum.com>
2011-03-23 19:32:52 +0800

    sync sc8800g_defconfig & sc8800g-trusted-abs-android_defconfig
    Change-Id: I3fd26edc08fef37c6ca1fb1df046ed1abd88a2c9

--- make_kernel.sh
+++ make_kernel.sh
@@ -12,7 +12,8 @@ BASE_DIR="${ANDROID_TOP_BASE}/out/target/product/hsdroid"
 
 cd ${ANDROID_TOP_BASE}
 
-make -f vendor/sprd/hsdroid/kernel.mk
+# make -f vendor/sprd/hsdroid/kernel.mk
+make -f vendor/sprd/hsdroid/kernel.mk EXTRAVERSION=-sprd-redbend CONFIG_DEBUG_SECTION_MISMATCH=y
 
 acp -fpt ${BASE_DIR}/obj/KERNEL/arch/arm/boot/Image ${BASE_DIR}/kernel
 
@@ -26,6 +27,23 @@ mkbootimg --kernel ${BASE_DIR}/kernel --ramdisk ${BASE_DIR}/ramdisk.img --cmdlin
 echo;echo;
 else
 mkbootimg --kernel ${BASE_DIR}/kernel --ramdisk ${BASE_DIR}/ramdisk.img --base 0x0 --output ${BASE_DIR}/boot.img
+fi
+
+if [ -f ${BASE_DIR}/obj/KERNEL/arch/arm/boot/Image -a -f ${BASE_DIR}/ramdisk.img ]; then
+cp ${BASE_DIR}/obj/KERNEL/arch/arm/boot/Image ${BASE_DIR}/linux.bin
+dd conv=notrunc if=${BASE_DIR}/ramdisk.img obs=32k seek=$(( (0x700000 / 0x8000) - 1 )) of=${BASE_DIR}/linux.bin
+echo 1>&2
+echo 1>&2
+echo "Createing ok -- ${BASE_DIR}/linux.bin" 1>&2
+echo 1>&2
+echo 1>&2
+else
+echo 1>&2
+echo 1>&2
+echo "Error: no ${BASE_DIR}/ramdisk.img is found!" 1>&2
+echo "Error: no ${BASE_DIR}/obj/KERNEL/arch/arm/boot/Image is found!" 1>&2
+echo 1>&2
+echo 1>&2
 fi
 
 cp -f ${BASE_DIR}/boot.img /data/ 2>/dev/null
