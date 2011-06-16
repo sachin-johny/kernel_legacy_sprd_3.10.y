154a7c3e736d3e4e76606caf9bb516861959f80a
gelixin <luther.ge@spreadtrum.com>
2011-06-16 15:51:14 +0800

    add LineinFM for FM
    Change-Id: I207bd89b7fe8aca1c63509f94b09b1b3e4dfde15

--- sound/soc/sc88xx/vbc-codec.c
+++ sound/soc/sc88xx/vbc-codec.c
@@ -111,6 +111,8 @@ static const struct snd_kcontrol_new vbc_snd_controls[] = {
     SOC_DOUBLE_R_TLV("BypassFM Playback Volume", VBCGR2, VBCGR3, 0, 0x0f, 1, dac_tlv),
     SOC_SINGLE_TLV("BypassFM Left Playback Volume", VBCGR2, 0, 0x0f, 1, dac_tlv),
     SOC_SINGLE_TLV("BypassFM Right Playback Volume",VBCGR3, 0, 0x0f, 1, dac_tlv),
+    // Linein
+    SOC_SINGLE("LineinFM", VBPMR1, SB_LIN, 1, 1),
     // Headset
     SOC_SINGLE("Headset Playback Switch", VBCR1, HP_DIS, 1, 1),
     SOC_DOUBLE_TLV("Headset Playback Volume", VBCGR1, 0, 4, 0x0f, 1, dac_tlv),
