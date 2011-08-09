786937146aae4047c8e8d977bd0f1310bc928212
gelixin <luther.ge@spreadtrum.com>
2011-08-09 11:09:18 +0800

    merge CSR update 1. speedup tx & rx in drivers/spi/spi_sc88xx.c for SPI_TMOD_CSR
    Change-Id: I385ca7b459482ad1d98f54cf334387d48224928a

--- drivers/spi/spi_sc88xx.c
+++ drivers/spi/spi_sc88xx.c
@@ -170,17 +170,22 @@ static int sprd_spi_direct_transfer_rx(void *data_in, const void *data_out, int 
   int i,j,timeout, block;
   int tlen = 0;
   int block_bytes = 128; //MAX 128
-  int saferx = 1;//2;
+  int saferx = min(len,8);
   unsigned char* data;
 
 #define MYLOCAL_TIMEOUT 0xff0000
-    struct sprd_spi_data *sprd_data = cookie;
-    // struct sprd_spi_controller_data *sprd_ctrl_data = cookie2;
 
+  struct sprd_spi_data *sprd_data = cookie;
+  // struct sprd_spi_controller_data *sprd_ctrl_data = cookie2;
+  
   data = (unsigned char *)data_in;
-  /* Enable rx only */
-  spi_write_reg(SPI_CTL1, 12, 0x01, 0x03); 
 
+  /* Set rx watermark */
+  spi_writel(saferx, SPI_CTL3);
+
+  /* Enable rx only */
+  spi_write_reg(SPI_CTL1, 12, 0x01, 0x03);
+  
   for (i = 0, tlen = len; tlen;i++) {
     if (tlen > block_bytes) {
       block = block_bytes;
@@ -192,23 +197,33 @@ static int sprd_spi_direct_transfer_rx(void *data_in, const void *data_out, int 
     
     spi_writel(0x0000, SPI_CTL4); /* stop only rx */
     spi_writel((1 << 9) | block, SPI_CTL4);
-    
+
     for (j = 0; j < block/saferx; j++) {
-      /* wait for rx fifo not empty */
       int k;
-      for (timeout = 0;(spi_readl(SPI_STS2) & SPI_RX_FIFO_REALLY_EMPTY) && timeout++ < MYLOCAL_TIMEOUT;);
+      
+      /* wait for rx fifo above watermark */
+      for (timeout = 0;!(spi_readl(SPI_STS2) & SPI_RX_FIFO_FULL) && timeout++ < MYLOCAL_TIMEOUT;);
+#if SPRD_SPI_DEBUG
+      if(timeout==MYLOCAL_TIMEOUT)printk("SPI: rx timeout: %i\n", timeout);
+#endif
+      
       /* Read the data */
       for(k=0;k<saferx;k++)
 	data[i*block_bytes+j*saferx+k] = spi_readl(SPI_TXD);
     }
-
+    
     for (j = 0; j < block%saferx; j++) {
-      /* wait for rx fifo not empty */
+      /* wait for rx fifo above watermark */
       for (timeout = 0;(spi_readl(SPI_STS2) & SPI_RX_FIFO_REALLY_EMPTY) && timeout++ < MYLOCAL_TIMEOUT;);
-      /* Read the data */
+#if SPRD_SPI_DEBUG
+      if(timeout==MYLOCAL_TIMEOUT) printk("SPI: rx timeout, frag\n");
+#endif
       data[i*block_bytes+(block/saferx)*saferx + j] = spi_readl(SPI_TXD);
     }
   }
+  
+  spi_writel(0x01 | (0x00 << 8), SPI_CTL3);
+
   return 0;
 }
 #endif
