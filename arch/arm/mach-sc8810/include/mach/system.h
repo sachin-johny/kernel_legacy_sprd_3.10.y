47795a179fbb9f905a517f2c72150757446314f0
mingwei.zhang <mingwei.zhang@spreadtrum.com>
2011-12-26 01:12:51 -0800

    add reboot code
    Change-Id: I2d8a7908982f27beb173a816b9975927ca399e1f

--- arch/arm/mach-sc8810/include/mach/system.h
+++ arch/arm/mach-sc8810/include/mach/system.h
@@ -15,6 +15,10 @@
 
 #include <asm/proc-fns.h>
 #include <mach/hardware.h>
+#include <mach/regs_wdg.h>
+#include <mach/adi_hal_internal.h>
+#include <mach/regs_ana.h>
+
 
 void cpu_idle_asm(void);
 
@@ -26,8 +30,40 @@ void arch_idle(void)
 #endif
 }
 
+#define ANA_WDG_LOAD_TIMEOUT_NUM    (10000)
+
+#define WDG_LOAD_TIMER_VALUE(value) \
+        do{\
+                    uint32_t   cnt          =  0;\
+                    ANA_REG_SET( WDG_LOAD_HIGH, (uint16_t)(((value) >> 16 ) & 0xffff));\
+                    ANA_REG_SET( WDG_LOAD_LOW , (uint16_t)((value)  & 0xffff) );\
+                    while((ANA_REG_GET(WDG_INT_RAW) & WDG_LD_BUSY_BIT) && ( cnt < ANA_WDG_LOAD_TIMEOUT_NUM )) cnt++;\
+                }while(0)
+
+#define HWRST_STATUS_RECOVERY (0x20)
+#define HWRST_STATUS_NORMAL (0X40)
+#define HWRST_STATUS_ALARM (0X50)
+#define HWRST_STATUS_SLEEP (0X60)
 
 static inline void arch_reset(char mode, const char *cmd)
 {
 	/* our chip reset code */
+    volatile int i;
+    for(i=0xffff; i>0;i--);
+    if(!(strncmp(cmd, "recovery", 8))){
+       ANA_REG_SET(ANA_RST_STATUS, HWRST_STATUS_RECOVERY);
+	}else if(!strncmp(cmd, "alarm", 5)){
+       ANA_REG_SET(ANA_RST_STATUS, HWRST_STATUS_ALARM);
+	}else if(!strncmp(cmd, "fastsleep", 9)){
+       ANA_REG_SET(ANA_RST_STATUS, HWRST_STATUS_SLEEP);
+    }else{
+        ANA_REG_SET(ANA_RST_STATUS, HWRST_STATUS_NORMAL);
+    }
+    // turn on watch dog clock
+    ANA_REG_OR(ANA_AGEN, AGEN_WDG_EN | AGEN_RTC_ARCH_EN | AGEN_RTC_WDG_EN);
+    ANA_REG_SET (WDG_LOCK, WDG_UNLOCK_KEY);
+    ANA_REG_AND (WDG_CTRL, (~WDG_INT_EN_BIT));
+    WDG_LOAD_TIMER_VALUE(0x50);
+    ANA_REG_OR (WDG_CTRL, WDG_CNT_EN_BIT);
+    ANA_REG_SET (WDG_LOCK, (~WDG_UNLOCK_KEY));
 }
