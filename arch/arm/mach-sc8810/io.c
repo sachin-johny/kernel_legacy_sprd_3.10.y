75991b3dfb9fcafa34a0bbcbf68e148115eb63c6
Geng Ren <geng.ren@spreadtrum.com>
2012-03-15 15:35:11 +0800

    Add io mapping implementation for sc8810
    Change-Id: I1c4491ed792cbbc7a68d5eca0e344881013bf6bf

--- arch/arm/mach-sc8810/io.c
+++ arch/arm/mach-sc8810/io.c
@@ -14,6 +14,70 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <asm/io.h>
+#include <asm/page.h>
+#include <asm/mach/map.h>
+#include <mach/hardware.h>
+
+#define SPRD_DEVICE(name) { \
+	.virtual = SPRD_##name##_BASE, \
+	.pfn = __phys_to_pfn(SPRD_##name##_PHYS), \
+	.length = SPRD_##name##_SIZE, \
+	.type = MT_DEVICE_NONSHARED, \
+	}
+
+static struct map_desc sprd_io_desc[] __initdata = {
+	SPRD_DEVICE(INTCV),
+	SPRD_DEVICE(DMA),
+	SPRD_DEVICE(ISP),
+	SPRD_DEVICE(USB),
+	SPRD_DEVICE(BUSM0),
+	SPRD_DEVICE(BUSM1),
+	SPRD_DEVICE(SDIO0),
+	SPRD_DEVICE(SDIO1),
+	SPRD_DEVICE(LCDC),
+	SPRD_DEVICE(ROTO),
+	SPRD_DEVICE(AHB),
+	SPRD_DEVICE(AXIM),
+	SPRD_DEVICE(EMC),
+	SPRD_DEVICE(DRM),
+	SPRD_DEVICE(MEA),
+	SPRD_DEVICE(NAND),
+	SPRD_DEVICE(PCMCIA),
+	SPRD_DEVICE(ASHB),
+	SPRD_DEVICE(TIMER),
+	SPRD_DEVICE(VB),
+	SPRD_DEVICE(SERIAL0),
+	SPRD_DEVICE(SERIAL1),
+	SPRD_DEVICE(SIM0),
+	SPRD_DEVICE(SIM1),
+	SPRD_DEVICE(I2C0),
+	SPRD_DEVICE(I2C1),
+	SPRD_DEVICE(I2C2),
+	SPRD_DEVICE(I2C3),
+	SPRD_DEVICE(SPI0),
+	SPRD_DEVICE(SPI1),
+	SPRD_DEVICE(KPD),
+	SPRD_DEVICE(SYSCNT),
+	SPRD_DEVICE(PWM),
+	SPRD_DEVICE(RTC),
+	SPRD_DEVICE(WDG),
+	SPRD_DEVICE(GPIO),
+	SPRD_DEVICE(GREG),
+	SPRD_DEVICE(CPC),
+	SPRD_DEVICE(EPT),
+	SPRD_DEVICE(SERIAL2),
+	SPRD_DEVICE(MISC),
+	SPRD_DEVICE(EIC),
+	SPRD_DEVICE(IIS0),
+	SPRD_DEVICE(IIS1),
+	SPRD_DEVICE(PMU),
+	SPRD_DEVICE(AXIM_GPU),
+	SPRD_DEVICE(CACHE310),
+	SPRD_DEVICE(A5_DEBUG),
+};
+
 void __init sc8810_map_io(void)
 {
+	iotable_init(sprd_io_desc, ARRAY_SIZE(sprd_io_desc));
 }
