be628ab8f19b5b33651fbb90f404e65ffa74ad0a
Chen Gaopeng <roc.chen@spreadtrum.com>
2012-03-29 16:25:48 +0800

    don't update config if .config is newer than defconfig directly copy ko files to system/lib/modules
    Change-Id: Iad8064fd9eaa42825acdf106fc67d5374bb0a45d

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -1,9 +1,8 @@
 
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL
-
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
-KERNEL_HEADERS_OUT := $(KERNEL_OUT)/usr
-KERNEL_MODULES_OUT := system
+KERNEL_MODULES_OUT := $(TARGET_OUT)/lib/modules
+
 ifeq ($(USES_UNCOMPRESSED_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/Image
 else
@@ -11,16 +10,17 @@ TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
 endif
 
 $(KERNEL_OUT):
-	mkdir -p $(KERNEL_OUT)
+	@echo "==== Start Kernel Compiling ... ===="
 
-$(KERNEL_CONFIG): $(KERNEL_OUT) kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
+$(KERNEL_CONFIG): kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	mkdir -p $(KERNEL_OUT)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- $(KERNEL_DEFCONFIG)
 
-$(KERNEL_HEADERS_OUT): $(KERNEL_OUT) $(KERNEL_CONFIG)
+$(TARGET_PREBUILT_KERNEL) : $(KERNEL_OUT) $(KERNEL_CONFIG)
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules_prepare
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install
-
-$(TARGET_PREBUILT_KERNEL) : $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_OUT)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- zImage -j4
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules_install
+	@-mkdir -p $(KERNEL_MODULES_OUT)
+	@-find $(KERNEL_OUT) -name *.ko | xargs -I{} cp {} $(KERNEL_MODULES_OUT)
 
