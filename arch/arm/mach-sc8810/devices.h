fba8d4069c0d508a2ead9a8339cc5c41647ce767
allen.mao <allen.mao@spreadtrum.com>
2012-04-02 18:42:41 -0400

    sprd fb init driver
    Change-Id: Ifa98fd59570ae567be0d88c0e0fd206d1f178a9f

--- arch/arm/mach-sc8810/devices.h
+++ arch/arm/mach-sc8810/devices.h
@@ -20,5 +20,6 @@ extern struct platform_device sprd_serial_device2;
 extern struct platform_device sprd_serial_device3;
 extern struct platform_device sprd_device_rtc;
 extern struct platform_device sprd_nand_device;
+extern struct platform_device sprd_lcd_device;
 
 #endif
