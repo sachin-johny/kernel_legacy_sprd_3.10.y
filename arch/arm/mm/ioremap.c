ab63a35b36d2d110223fa23456792beadc4f5279
Chen Gaopeng <roc.chen@spreadtrum.com>
2012-04-17 16:32:50 +0800

    workaround PMEM iomap issue
    Change-Id: I20c0bd0b95cb963ba2b231628da38b89aeef6004

--- arch/arm/mm/ioremap.c
+++ arch/arm/mm/ioremap.c
@@ -201,11 +201,13 @@ void __iomem * __arm_ioremap_pfn_caller(unsigned long pfn,
 	if (pfn >= 0x100000 && (__pfn_to_phys(pfn) & ~SUPERSECTION_MASK))
 		return NULL;
 
+#ifndef CONFIG_ANDROID_PMEM
 	/*
 	 * Don't allow RAM to be mapped - this causes problems with ARMv6+
 	 */
 	if (WARN_ON(pfn_valid(pfn)))
 		return NULL;
+#endif
 
 	type = get_mem_type(mtype);
 	if (!type)
