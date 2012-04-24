b6cf91372bdc019d1f91afee174323bf695fd508
alex.wang <alex.wang@spreadtrum.com>
2012-04-24 19:58:59 +0800

    [bug number] no [root cause] CTS failed [changes]    modify /sys/devices/system/cpu/cpu0/cpufreq/input_event, 666 ---> 644 [side effects] [reviewers]
    	modified:   cpufreq.c
    
    Change-Id: I1d577c662caab2ccaff8cfbfff42a8a887fbe4a1

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -708,7 +708,8 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-cpufreq_freq_attr_rwrwrw(input_event);//wong
+//cpufreq_freq_attr_rwrwrw(input_event);//wong
+cpufreq_freq_attr_rw(input_event);//wong
 
 
 static struct attribute *default_attrs[] = {
