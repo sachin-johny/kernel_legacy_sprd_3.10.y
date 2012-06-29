57a8de88d7fd14f012596fddeb5cc844481eb99c
allen.zhang <allen.zhang@spreadtrum.com>
2012-06-29 14:00:30 +0800

    Bug#4337 add brcm gps
    Change-Id: I6ab0d7058cc33c51f3ee484b2e3eb1864e67e4ca

--- arch/arm/mach-sc8810/devices.h
+++ arch/arm/mach-sc8810/devices.h
@@ -26,6 +26,7 @@ extern struct platform_device sprd_backlight_device;
 extern struct platform_device sprd_i2c_device0;
 extern struct platform_device sprd_i2c_device1;
 extern struct platform_device sprd_i2c_device2;
+extern struct platform_device sprd_i2c_device3;
 extern struct platform_device sprd_spi0_device;
 extern struct platform_device sprd_spi1_device;
 extern struct platform_device sprd_keypad_device;
