0334014772aea60a34282b21a4ef7bc338ff8faa
zhaiyafei <zhaiyf@spreadst.com>
2012-07-25 11:54:03 +0800

    Bug#6566 Alarm does not ring after sleep
    [bug number  ]6566
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]mark.yang
    
    Change-Id: I3de70559f7307c79d214d3241b51d0f29fd699d9

--- drivers/rtc/rtc-sprd.c
+++ drivers/rtc/rtc-sprd.c
@@ -336,9 +336,9 @@ static int sprd_rtc_open(struct device *dev)
 {
 	int temp = 0;
 	/* enable rtc interrupt */
-	temp = ANA_REG_GET(ANA_RTC_INT_EN);
+	temp = sci_adi_read(ANA_RTC_INT_EN);
 	temp |= RTC_ALARM_BIT;
-	ANA_REG_SET(ANA_RTC_INT_EN, temp);
+	sci_adi_raw_write(ANA_RTC_INT_EN, temp);
 	return 0;
 }
 
