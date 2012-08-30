c0a8e11d589c6f6f5d550834d604c8c34486fee9
steve.zhan <steve.zhan@spreadtrum.com>
2012-08-30 20:26:02 +0800

    ARM: SC8825: adc:  add adc new driver;
            change adc interface, define default interface to suport old
    version interface; change adie related init code;
    
    Change-Id: I5cc0f2dfc3c4f880f16c79a3f15495537d25f6c9

--- arch/arm/mach-sc8825/adc.c
+++ arch/arm/mach-sc8825/adc.c
@@ -13,6 +13,7 @@
 
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/err.h>
 #include <linux/module.h>
 #include <linux/irqflags.h>
 #include <linux/delay.h>
@@ -21,68 +22,16 @@
 #include <mach/adi.h>
 #include <mach/adc.h>
 
-#define ANA_CTL_ADC_BASE		(SPRD_MISC_BASE + 0x0300)
+static DEFINE_SPINLOCK(adc_lock);
+static u32 io_base;		/* Mapped base address */
 
-/* registers definitions for controller ANA_CTL_ADC */
-#define ANA_REG_ADC_CTRL                (ANA_CTL_ADC_BASE + 0x0000)
-#define ANA_REG_ADC_CS                  (ANA_CTL_ADC_BASE + 0x0004)
-#define ANA_REG_ADC_TPC_CH_CTRL         (ANA_CTL_ADC_BASE + 0x0008)
-#define ANA_REG_ADC_DAT                 (ANA_CTL_ADC_BASE + 0x000c)
-#define ANA_REG_ADC_IE                  (ANA_CTL_ADC_BASE + 0x0010)
-#define ANA_REG_ADC_IC                  (ANA_CTL_ADC_BASE + 0x0014)
-#define ANA_REG_ADC_ISTS                (ANA_CTL_ADC_BASE + 0x0018)
-#define ANA_REG_ADC_ISRC                (ANA_CTL_ADC_BASE + 0x001c)
-
-/* bits definitions for register REG_ADC_CTRL */
-#define BIT_ADC_STATUS                  ( BIT(4) )
-#define BIT_HW_INT_EN                   ( BIT(3) )
-#define BIT_TPC_CH_ON                   ( BIT(2) )
-#define BIT_SW_CH_ON                    ( BIT(1) )
-#define BIT_ADC_EN                      ( BIT(0) )
-
-/* bits definitions for register REG_ADC_CS */
-#define BIT_ADC_SLOW                    ( BIT(5) )
-#define BIT_ADC_SCALE                   ( BIT(4) )
-#define BITS_ADC_CS(_x_)                ( (_x_) & 0x0F )
-
-#define SHIFT_ADC_CS                    ( 0 )
-#define MASK_ADC_CS                     ( 0x0F )
-
-/* bits definitions for register REG_ADC_TPC_CH_CTRL */
-#define BITS_TPC_CH_DELAY(_x_)          ( (_x_) << 8 & 0xFF00 )
-#define BITS_TPC_Y_CH(_x_)              ( (_x_) << 4 & 0xF0 )
-#define BITS_TPC_X_CH(_x_)              ( (_x_) << 0 & 0x0F )
-
-/* bits definitions for register REG_ADC_DAT */
-#define BITS_ADC_DAT(_x_)               ( (_x_) << 0 & 0x3FF )
-
-#define SHIFT_ADC_DAT                   ( 0 )
-#define MASK_ADC_DAT                    ( 0x3FF )
-
-/* bits definitions for register REG_ADC_IE */
-#define BIT_ADC_IE                      ( BIT(0) )
-
-/* bits definitions for register REG_ADC_IC */
-#define BIT_ADC_IC                      ( BIT(0) )
-
-/* bits definitions for register REG_ADC_ISTS */
-#define BIT_ADC_MIS                     ( BIT(0) )
-
-/* bits definitions for register REG_ADC_ISRC */
-#define BIT_ADC_RIS                     ( BIT(0) )
-
-/* adc global regs */
-#define ANA_REG_GLB_APB_CLK_EN		(SPRD_MISC_BASE + 0x0600)
-#define BIT_CLK_AUXAD_EN                ( BIT(14) )
-#define BIT_CLK_AUXADC_EN               ( BIT(13) )
-#define BIT_ADC_EB                      ( BIT(5) )
-
-void sci_adc_enable(void)
+#define adc_write(val,reg) \
+	do { \
+		sci_adi_raw_write((u32)reg,val); \
+} while (0)
+static unsigned adc_read(unsigned addr)
 {
-	/* enable adc */
-	sci_adi_set(ANA_REG_GLB_APB_CLK_EN,
-			BIT_ADC_EB | BIT_CLK_AUXADC_EN | BIT_CLK_AUXAD_EN);
-	sci_adi_set(ANA_CTL_ADC_BASE, BIT_ADC_EN);
+	return sci_adi_read(addr);
 }
 
 #ifdef CONFIG_NKERNEL
@@ -92,47 +41,174 @@ void sci_adc_enable(void)
 		hw_local_irq_restore(flags)
 
 #else
-#define sci_adc_lock()
-#define sci_adc_unlock()
+#define sci_adc_lock() do {spin_lock_irqsave(&adc_lock,flags);} while(0)
+#define sci_adc_unlock() do {spin_unlock_irqrestore(&adc_lock,flags);} while(0)
 
 #endif
 
-int sci_adc_get_value(unsigned int channel, int scale)
+#define ADC_CTL		(0x00)
+#define ADC_SW_CH_CFG		(0x04)
+#define ADC_FAST_HW_CHX_CFG(_X_)		((_X_) * 0x4 + 0x8)
+#define ADC_SLOW_HW_CHX_CFG(_X_)		((_X_) * 0x4 + 0x28)
+#define ADC_HW_CH_DELAY		(0x48)
+
+#define ADC_DAT		(0x4c)
+#define adc_get_data(_SAMPLE_BITS_)		(adc_read(io_base + ADC_DAT) & (_SAMPLE_BITS_))
+
+#define ADC_IRQ_EN		(0x50)
+#define adc_enable_irq(_X_)	do {adc_write(((_X_) & 0x1),io_base + ADC_IRQ_EN);} while(0)
+
+#define ADC_IRQ_CLR		(0x54)
+#define adc_clear_irq()		do {adc_write(0x1, io_base + ADC_IRQ_CLR);} while (0)
+
+#define ADC_IRQ_STS		(0x58)
+#define adc_mask_irqstatus()     adc_read(io_base + ADC_IRQ_STS)
+
+#define ADC_IRQ_RAW		(0x5c)
+#define adc_raw_irqstatus()     adc_read(io_base + ADC_IRQ_RAW)
+
+#define ADC_DEBUG		(0x60)
+
+/*ADC_CTL */
+#define BIT_SW_CH_RUN_NUM(_X_)		((((_X_) - 1) & 0xf ) << 4)
+#define BIT_ADC_BIT_MODE(_X_)		(((_X_) & 0x1) << 2)	/*0: adc in 10bits mode, 1: adc in 12bits mode */
+#define BIT_ADC_BIT_MODE_MASK		BIT_ADC_BIT_MODE(1)
+#define BIT_SW_CH_ON                    ( BIT(1) ) /*WO*/
+#define BIT_ADC_EN                      ( BIT(0) )
+/*ADC_SW_CH_CFG && ADC_FAST(SLOW)_HW_CHX_CFG*/
+#define BIT_CH_IN_MODE(_X_)		(((_X_) & 0x1) << 8)	/*0: resistance path, 1: capacitance path */
+#define BIT_CH_SLOW(_X_)		(((_X_) & 0x1) << 6)	/*0: quick mode, 1: slow mode */
+#define BIT_CH_SCALE(_X_)		(((_X_) & 0x1) << 5)	/*0: little scale, 1: big scale */
+#define BIT_CH_ID(_X_)			((_X_) & 0x1f)
+/*ADC_FAST(SLOW)_HW_CHX_CFG*/
+#define BIT_CH_DLY_EN(_X_)		(((_X_) & 0x1) << 7)	/*0:disable, 1:enable */
+/*ADC_HW_CH_DELAY*/
+#define BIT_HW_CH_DELAY(_X_)		((_X_) & 0xff)	/*its unit is ADC clock */
+#define BIT_ADC_EB                  ( BIT(5) )
+#define BIT_CLK_AUXADC_EN                      ( BIT(13) )
+#define BIT_CLK_AUXAD_EN						( BIT(14) )
+static void sci_adc_enable(void)
+{
+	sci_adi_set(ANA_REG_GLB_APB_CLK_EN,
+		    BIT_ADC_EB | BIT_CLK_AUXADC_EN | BIT_CLK_AUXAD_EN);
+}
+
+void sci_adc_dump_register()
+{
+	unsigned _base = (unsigned)(io_base);
+	unsigned _end = _base + 0x64;
+
+	printk("sci_adc_dump_register begin\n");
+	for (; _base < _end; _base += 4) {
+		printk("_base = 0x%x, value = 0x%x\n", _base, adc_read(_base));
+	}
+	printk("sci_adc_dump_register end\n");
+}
+
+EXPORT_SYMBOL(sci_adc_dump_register);
+
+void sci_adc_init(void __iomem * adc_base)
+{
+	io_base = (u32) adc_base;
+	adc_enable_irq(0);
+	adc_clear_irq();
+	sci_adc_enable();
+}
+
+EXPORT_SYMBOL(sci_adc_init);
+
+/*
+*	Notes: for hw channel,  config its hardware configuration register and using sw channel to read it.
+*/
+static int sci_adc_config(struct adc_sample_data *adc)
+{
+	unsigned addr = 0;
+	unsigned val = 0;
+	int ret = 0;
+
+	BUG_ON(!adc);
+	BUG_ON(adc->channel_id > ADC_MAX);
+
+	val = BIT_CH_IN_MODE(adc->signal_mode);
+	val |= BIT_CH_SLOW(adc->sample_speed);
+	val |= BIT_CH_SCALE(adc->scale);
+	val |= BIT_CH_ID(adc->channel_id);
+	val |= BIT_CH_DLY_EN(adc->hw_channel_delay ? 1 : 0);
+
+	adc_write(val, io_base + ADC_SW_CH_CFG);
+
+	if (adc->channel_type > 0) {	/*hardware */
+		adc_write(BIT_HW_CH_DELAY(adc->hw_channel_delay),
+			  io_base + ADC_HW_CH_DELAY);
+
+		if (adc->channel_type == 1) {	/*slow */
+			addr = io_base + ADC_SLOW_HW_CHX_CFG(adc->channel_id);
+		} else {
+			addr = io_base + ADC_FAST_HW_CHX_CFG(adc->channel_id);
+		}
+		adc_write(val, addr);
+	}
+
+	return ret;
+}
+
+int sci_adc_get_values(struct adc_sample_data *adc)
 {
 	unsigned long flags;
-	int ret, cnt = 12;
+	int cnt = 12;
+	unsigned addr = 0;
+	unsigned val = 0;
+	int ret = 0;
+	int num = 0;
+	int sample_bits_msk = 0;
+	int *pbuf = adc->pbuf;
 
-	BUG_ON(channel >= ADC_MAX);
-
-	if (scale)
-		scale = BIT_ADC_SCALE;
+	num = adc->sample_num;
+	BUG_ON(adc->channel_id > ADC_MAX || !pbuf);
 
 	sci_adc_lock();
 
-	/* clear int */
-	sci_adi_set(ANA_REG_ADC_IC, BIT_ADC_IC);
+	sci_adc_config(adc);	//configs adc sample.
 
-	sci_adi_raw_write(ANA_REG_ADC_CS, channel | scale);
+	addr = io_base + ADC_CTL;
+	val = adc_read(addr);
+	val &= ~(BIT_ADC_EN | BIT_SW_CH_ON | BIT_ADC_BIT_MODE_MASK);
+	adc_write(val, addr);
 
-	/* turn on sw channel */
-	sci_adi_set(ANA_REG_ADC_CTRL, BIT_SW_CH_ON);
+	adc_clear_irq();
 
-	/* wait adc complete */
-	while (!(sci_adi_read(ANA_REG_ADC_ISRC) & BIT_ADC_RIS) && cnt--) {
+	val = BIT_SW_CH_RUN_NUM(num);
+	val |= BIT_ADC_EN;
+	val |= BIT_ADC_BIT_MODE(adc->sample_bits);
+	val |= BIT_SW_CH_ON;
+
+	adc_write(val, addr);
+
+	while ((!adc_raw_irqstatus()) && cnt--) {
 		udelay(50);
 	}
 
-	WARN_ON(!cnt);
+	if (!cnt) {
+		ret = -1;
+		WARN_ON(1);
+		goto Exit;
+	}
 
-	ret = sci_adi_read(ANA_REG_ADC_DAT) & MASK_ADC_DAT;
+	if (adc->sample_bits)
+		sample_bits_msk = ((1 << 12) - 1);	//12
+	else
+		sample_bits_msk = ((1 << 10) - 1);	//10
+	while (num--)
+		*pbuf++ = adc_get_data(sample_bits_msk);
 
-	/* turn off sw channel */
-	sci_adi_clr(ANA_REG_ADC_CTRL, BIT_SW_CH_ON);
-
-	/* clear int */
-	sci_adi_set(ANA_REG_ADC_IC, BIT_ADC_IC);
+Exit:
+	val = adc_read(addr);
+	val &= ~BIT_ADC_EN;
+	adc_write(val, addr);
 
 	sci_adc_unlock();
 
 	return ret;
 }
+
+EXPORT_SYMBOL_GPL(sci_adc_get_values);
