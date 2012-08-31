c0417b69eae3c911a592ffc3c1ba380edac7a19f
steve.zhan <steve.zhan@spreadtrum.com>
2012-08-31 21:48:09 +0800

    ARM: sc8825: DMA: porting DMA driver from SC8810
    Change-Id: I759d6ed9ee1e3e8dd45d99808fe154108db1c252

--- arch/arm/mach-sc8825/include/mach/dma.h
+++ arch/arm/mach-sc8825/include/mach/dma.h
@@ -13,9 +13,8 @@
 
 #ifndef __ASM_ARCH_SPRD_DMA_H
 
-#include <asm/io.h>
-#include <linux/list.h>
 #include <linux/interrupt.h>
+#include <asm/io.h>
 #include <mach/hardware.h>
 #include <mach/globalregs.h>
 
@@ -23,7 +22,68 @@
 **                               Micro Define                                 **
 **----------------------------------------------------------------------------*/
 #define DMA_REG_BASE                    SPRD_DMA0_BASE
-#define DMA_CHx_CTL_BASE                (DMA_REG_BASE + 0x0400)
+
+/* 0X00 */
+#define DMA_CFG                         (DMA_REG_BASE + 0x0000)
+#define DMA_CHN_EN_STATUS               (DMA_REG_BASE + 0x0004)
+#define DMA_LINKLIST_EN                 (DMA_REG_BASE + 0x0008)
+#define DMA_SOFTLINK_EN                 (DMA_REG_BASE + 0x000C)
+
+/* 0X10 */
+#define DMA_SOFTLIST_SIZE               (DMA_REG_BASE + 0x0010)
+#define DMA_SOFTLIST_CMD                (DMA_REG_BASE + 0x0014)
+#define DMA_SOFTLIST_STS                (DMA_REG_BASE + 0x0018)
+#define DMA_SOFTLIST_BASEADDR           (DMA_REG_BASE + 0x001C)
+
+/* 0X20 */
+#define DMA_PRI_REG0                    (DMA_REG_BASE + 0x0020)
+#define DMA_PRI_REG1                    (DMA_REG_BASE + 0x0024)
+
+/* 0X30 */
+#define DMA_INT_STS                     (DMA_REG_BASE + 0x0030)
+#define DMA_INT_RAW                     (DMA_REG_BASE + 0x0034)
+
+/* 0X40 */
+#define DMA_LISTDONE_INT_EN             (DMA_REG_BASE + 0x0040)
+#define DMA_BURST_INT_EN                (DMA_REG_BASE + 0x0044)
+#define DMA_TRANSF_INT_EN               (DMA_REG_BASE + 0x0048)
+
+/* 0X50 */
+#define DMA_LISTDONE_INT_STS            (DMA_REG_BASE + 0x0050)
+#define DMA_BURST_INT_STS               (DMA_REG_BASE + 0x0054)
+#define DMA_TRANSF_INT_STS              (DMA_REG_BASE + 0x0058)
+
+/* 0X60 */
+#define DMA_LISTDONE_INT_RAW            (DMA_REG_BASE + 0x0060)
+#define DMA_BURST_INT_RAW               (DMA_REG_BASE + 0x0064)
+#define DMA_TRANSF_INT_RAW              (DMA_REG_BASE + 0x0068)
+
+/* 0X70 */
+#define DMA_LISTDONE_INT_CLR            (DMA_REG_BASE + 0x0070)
+#define DMA_BURST_INT_CLR               (DMA_REG_BASE + 0x0074)
+#define DMA_TRANSF_INT_CLR              (DMA_REG_BASE + 0x0078)
+
+/* 0X80 */
+#define DMA_SOFT_REQ                    (DMA_REG_BASE + 0x0080)
+#define DMA_TRANS_STS                   (DMA_REG_BASE + 0x0084)
+#define DMA_REQ_PEND                    (DMA_REG_BASE + 0x0088)
+
+/* 0X90 */
+#define DMA_WRAP_START                  (DMA_REG_BASE + 0x0090)
+#define DMA_WRAP_END                    (DMA_REG_BASE + 0x0094)
+
+#define DMA_CHN_UID_BASE                (DMA_REG_BASE + 0x0098)
+#define DMA_CHN_UID0                    (DMA_REG_BASE + 0x0098)
+#define DMA_CHN_UID1                    (DMA_REG_BASE + 0x009C)
+#define DMA_CHN_UID2                    (DMA_REG_BASE + 0x00A0)
+#define DMA_CHN_UID3                    (DMA_REG_BASE + 0x00A4)
+#define DMA_CHN_UID4                    (DMA_REG_BASE + 0x00A8)
+#define DMA_CHN_UID5                    (DMA_REG_BASE + 0x00AC)
+#define DMA_CHN_UID6                    (DMA_REG_BASE + 0x00B0)
+#define DMA_CHN_UID7                    (DMA_REG_BASE + 0x00B4)
+
+#define DMA_CHx_EN                      (DMA_REG_BASE + 0x00C0)
+#define DMA_CHx_DIS                     (DMA_REG_BASE + 0x00C4)
 
 /* Chanel x dma contral regisers address offset*/
 #define DMA_CH_CFG0                     (0x0000)
@@ -36,6 +96,7 @@
 #define DMA_CH_DBP                      (0x001c)
 
 /* Channel x dma contral regisers address */
+#define DMA_CHx_CTL_BASE                (DMA_REG_BASE + 0x0400)
 #define DMA_CHx_BASE(x)                 (DMA_CHx_CTL_BASE + 0x20 * x )
 #define DMA_CHx_CFG0(x)                 (DMA_CHx_CTL_BASE + 0x20 * x + DMA_CH_CFG0)
 #define DMA_CHx_CFG1(x)                 (DMA_CHx_CTL_BASE + 0x20 * x + DMA_CH_CFG1)
@@ -77,8 +138,10 @@
 #define DMA_NFC_RX                      27
 #define DMA_DRM_RAW                     29
 #define DMA_DRM_CPT                     30
+
 #define DMA_UID_MIN                     0
 #define DMA_UID_MAX                     32
+
 #define DMA_UID_MASK                    0x1f
 #define DMA_UID_SHIFT_STP               8
 #define DMA_UID_UNIT                    4
@@ -136,10 +199,9 @@
 #define DMA_SOFT_WAITTIME              0x0f
 #define DMA_HARD_WAITTIME              0x0f
 
-#define DMA_INCREASE                   ( 0 )
-#define DMA_DECREASE                   ( 1 )
-#define DMA_NOCHANGE                   ( 0xff )
-
+#define DMA_INCREASE                   (0)
+#define DMA_DECREASE                   (1)
+#define DMA_NOCHANGE                   (0xff)
 
 #define ON                              1
 #define OFF                             0
@@ -159,108 +221,35 @@
 #define SOFTLIST_REQ_PTR_SHIFT          16
 #define SOFTLIST_CNT_MASK               0xffff
 
-
-/* DMA_LISTDONE_INT_EN, DMA_BURST_INT_EN, DMA_TRANSF_INT_EN */
-typedef enum{
-	LINKLIST_DONE,
-	BLOCK_DONE,
-	TRANSACTION_DONE,
-}dma_done_type;
-
-
-enum {
-	INT_NONE = 0x00,
-	LLIST_DONE_EN = 0x01,
-	BURST_DONE_EN = 0x02,
-	TRANS_DONE_EN = 0x04,
-};
-
-typedef enum {
-	/* For dma mode */
-	DMA_WRAP        = (1 << 0),
-	DMA_NORMAL      = (1 << 1),
-	DMA_LINKLIST    = (1 << 2),
-	DMA_SOFTLIST    = (1 << 3),
-}dma_work_mode;
-
-extern spinlock_t dma_lock;
-
-struct sprd_dma_channel_desc{
-	u32 cfg_swt_mode_sel;
-	u32 cfg_src_data_width;
-	u32 cfg_dst_data_width;
-	u32 cfg_req_mode_sel;/* request mode */
-	u32 cfg_src_wrap_en;
-	u32 cfg_dst_wrap_en;
-	u32 cfg_blk_len;
-	u32 total_len;
-	u32 src_addr;
-	u32 dst_addr;
-	u32 llist_ptr;/*linklist mode only, must be 8 words boundary */
-	u32 src_elem_postm;
-	u32 dst_elem_postm;
-	u32 src_burst_mode;
-	u32 src_blk_postm;
-	u32 dst_burst_mode;
-	u32 dst_blk_postm;
-};
-
-struct sprd_dma_softlist_desc{
-	u32 softlist_size;
-	u32 softlist_cnt_incr;
-	u32 softlist_req_ptr;
-	u32 softlist_cnt;
-	u32 softlist_base_addr;
-};
-
-struct sprd_dma_wrap_addr{
-	u32 wrap_start_addr;
-	u32 wrap_end_addr;
-};
-
-static inline void dma_reg_write(u32 reg, u8 shift, u32 val, u32 mask)
+static inline void reg_writel(u32 reg, u8 shift, u32 val, u32 mask)
 {
-	unsigned long flags;
 	u32 tmp;
-	spin_lock_irqsave(&dma_lock, flags);
 	tmp = __raw_readl(reg);
 	tmp &= ~(mask<<shift);
 	tmp |= val << shift;
 	__raw_writel(tmp, reg);
-	spin_unlock_irqrestore(&dma_lock, flags);
 }
 
+static inline void reg_bits_or(u32 bits, u32 reg)
+{
+	__raw_writel((__raw_readl(reg) | bits), reg);
+}
+
+static inline void reg_bits_and(u32 bits, u32 reg)
+{
+	__raw_writel((__raw_readl(reg) & bits), reg);
+}
+
+/* will be removed */
+#if 1
 static inline void dma_reg_bits_or(u32 bits, u32 reg)
 {
-	unsigned long flags;
-	spin_lock_irqsave(&dma_lock, flags);
 	__raw_writel((__raw_readl(reg) | bits), reg);
-	spin_unlock_irqrestore(&dma_lock, flags);
 }
-
-static inline void dma_reg_bits_and(u32 bits, u32 reg)
-{
-	unsigned long flags;
-	spin_lock_irqsave(&dma_lock, flags);
-	__raw_writel((__raw_readl(reg) & bits), reg);
-	spin_unlock_irqrestore(&dma_lock, flags);
-}
-
-static inline void dma_set_reg(u32 val, u32 reg)
-{
-	unsigned long flags;
-	spin_lock_irqsave(&dma_lock, flags);
-	__raw_writel(val, reg);
-	spin_unlock_irqrestore(&dma_lock, flags);
-}
-
 static inline u32 dma_get_reg(u32 reg)
 {
 	return __raw_readl(reg);
 }
-
-#define DMA_CHx_EN                      (DMA_REG_BASE + 0x00C0)
-#define DMA_CHx_DIS                     (DMA_REG_BASE + 0x00C4)
 
 #define sprd_dma_start(ch_id) \
     dma_reg_bits_or(1 << ch_id, DMA_CHx_EN) /* Enable DMA channel */
@@ -297,21 +286,89 @@ static inline u32 dma_get_reg(u32 reg)
 
 #define sprd_dma_dbm(ch_id, dbm) \
     __raw_writel(dbm,  DMA_CHx_CTL_BASE + (ch_id * 0x20) + 0x1C) /* dbm */
+#endif
+
+struct sprd_irq_handler {
+	irq_handler_t handler;
+	void *dev_id;
+	u32 dma_uid;
+	u32 used;/* mark the channel used before new API done */
+};
+
+/* DMA_LISTDONE_INT_EN, DMA_BURST_INT_EN, DMA_TRANSF_INT_EN */
+typedef enum{
+	LINKLIST_DONE,
+	BLOCK_DONE,
+	TRANSACTION_DONE,
+}dma_done_type;
 
 
-int  sprd_dma_request(u32 uid, void (*irq_handler)(int, void *), void *data);
+enum {
+	INT_NONE = 0x00,
+	LLIST_DONE_EN = 0x01,
+	BURST_DONE_EN = 0x02,
+	TRANS_DONE_EN = 0x04,
+};
+
+typedef enum {
+	/* For dma mode */
+	DMA_WRAP        = (1 << 0),
+	DMA_NORMAL      = (1 << 1),
+	DMA_LINKLIST    = (1 << 2),
+	DMA_SOFTLIST    = (1 << 3),
+}dma_work_mode;
+
+struct sprd_dma_channel_desc {
+	u32 cfg_swt_mode_sel;
+	u32 cfg_src_data_width;
+	u32 cfg_dst_data_width;
+	u32 cfg_req_mode_sel;/* request mode */
+	u32 cfg_src_wrap_en;
+	u32 cfg_dst_wrap_en;
+	u32 cfg_blk_len;
+
+	u32 total_len;
+	u32 src_addr;
+	u32 dst_addr;
+	u32 llist_ptr;/*linklist mode only, must be 8 words boundary */
+	u32 src_elem_postm;
+	u32 dst_elem_postm;
+	u32 src_burst_mode;
+	u32 src_blk_postm;
+	u32 dst_burst_mode;
+	u32 dst_blk_postm;
+};
+
+struct sprd_dma_linklist_desc {
+	u32 cfg;
+	u32 total_len;
+	u32 src_addr;
+	u32 dst_addr;
+	u32 llist_ptr;
+	u32 elem_postm;
+	u32 src_blk_postm;
+	u32 dst_blk_postm;
+};
+
+struct sprd_dma_wrap_addr {
+	u32 wrap_start_addr;
+	u32 wrap_end_addr;
+};
+
+/* those spreadtrum DMA interface must be implemented */
+int  sprd_dma_request(u32 uid, irq_handler_t irq_handle, void *data);
 void sprd_dma_free(u32 uid);
-void sprd_dma_chn_cfg_update(u32 chn, struct sprd_dma_channel_desc *desc);
-void sprd_dma_channel_config(u32 chn, dma_work_mode work_mode, struct sprd_dma_channel_desc *dma_cfg);
-void sprd_dma_softlist_config(struct sprd_dma_softlist_desc *softlist_desc);
-void sprd_dma_wrap_addr_config(struct sprd_dma_wrap_addr *wrap_addr);
+void sprd_dma_channel_config(u32 chn, dma_work_mode work_mode, const struct sprd_dma_channel_desc *dma_cfg);
+void sprd_dma_default_channel_setting(struct sprd_dma_channel_desc *dma_cfg);
+void sprd_dma_default_linklist_setting(struct sprd_dma_linklist_desc *chn_cfg);
+void sprd_dma_linklist_config(u32 chn_id, u32 dma_cfg);
+void sprd_dma_wrap_addr_config(const struct sprd_dma_wrap_addr *wrap_addr);
 void sprd_dma_set_irq_type(u32 chn, dma_done_type irq_type, u32 on_off);
 void sprd_dma_set_chn_pri(u32 chn, u32 pri);
 void sprd_dma_channel_start(u32 chn);
 void sprd_dma_channel_stop(u32 chn);
-int  sprd_dma_irq_handler_ready(u32 chn);
-
 /* ONLY FOR DEBUG */
 void sprd_dma_check_channel(void);
 void sprd_dma_dump_regs(void);
+
 #endif
