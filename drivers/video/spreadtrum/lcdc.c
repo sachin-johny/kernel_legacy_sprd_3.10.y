97b74b623a01f6577b02b7c79c122065d9c3cb1e
jessica.lu <jessica.lu@spreadtrum.com>
2012-09-11 17:01:11 -0400

    Bug#219196 Add code support overlay refresh synchronize
    [bug number  ]NEWMS00119196
    [root cause  ]If not use sync refresh, the display buffer will be changed by application.
    [changes     ]refresh sysnchronize
    [side effects]ui will be delay.
    [reviewers   ]hansen.sun jianguo.du
    
    Change-Id: I9cf248c76b2a8514b4e63a034cfb4cda6061ffbf

--- drivers/video/spreadtrum/lcdc.c
+++ drivers/video/spreadtrum/lcdc.c
@@ -722,6 +722,14 @@ static int32_t sprd_lcdc_display_overlay(struct sprdfb_device *dev, struct overl
 	/* start refresh */
 	lcdc_set_bits((1 << 3), LCDC_CTRL);
 
+#ifdef CONFIG_FB_OVERLAY_SYNC
+	dev->vsync_waiter ++;
+	if (dev->ctrl->sync(dev) != 0) {/* time out??? disable ?? */
+		dev->vsync_waiter = 0;
+		printk("sprdfb  do sprd_lcdc_display_overlay  time out!\n");
+	}
+#endif
+
 	up(&lcdc.overlay_lock);
 
 	pr_debug("LCDC_CTRL: 0x%x\n", lcdc_read(LCDC_CTRL));
