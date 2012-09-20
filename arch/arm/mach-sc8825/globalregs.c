957a8a1ab7a61deef2446d8cc25aac69b3e1fa78
robot <zhulin.lian@spreadtrum.com>
2012-09-20 16:03:35 -0400

    add sc8825 globalregs set/clear ops
    Change-Id: I4a82ed3c69bfe214dfd638c1444adeb51ee3b93f

--- arch/arm/mach-sc8825/globalregs.c
+++ arch/arm/mach-sc8825/globalregs.c
@@ -16,12 +16,14 @@
 #include <linux/spinlock.h>
 #include <linux/module.h>
 
+/* vars definitions for controller REGS_GLB */
+#define REG_GLB_SET(A)                  ( (A) + 0x1000 )
+#define REG_GLB_CLR(A)                  ( (A) + 0x2000 )
+
 static uint32_t globalregs[] = {
 	SPRD_GREG_BASE,
 	SPRD_AHB_BASE + 0x200,
 };
-
-static spinlock_t lock = __SPIN_LOCK_UNLOCKED(lock);
 
 int32_t sprd_greg_read(uint32_t type, uint32_t reg_offset)
 {
@@ -35,42 +37,12 @@ void sprd_greg_write(uint32_t type, uint32_t value, uint32_t reg_offset)
 
 void sprd_greg_set_bits(uint32_t type, uint32_t bits, uint32_t reg_offset)
 {
-	int value;
-	unsigned long flags;
-
-#ifndef CONFIG_NKERNEL
-	spin_lock_irqsave(&lock, flags);
-#else
-	flags = hw_local_irq_save();
-#endif
-	value = __raw_readl(globalregs[type] + reg_offset);
-	value |= bits;
-	__raw_writel(value, globalregs[type] + reg_offset);
-#ifndef CONFIG_NKERNEL
-	spin_unlock_irqrestore(&lock, flags);
-#else
-	hw_local_irq_restore(flags);
-#endif
+	__raw_writel(bits, REG_GLB_SET(globalregs[type] + reg_offset));
 }
 
 void sprd_greg_clear_bits(uint32_t type, uint32_t bits, uint32_t reg_offset)
 {
-	int value;
-	unsigned long flags;
-
-#ifndef CONFIG_NKERNEL
-	spin_lock_irqsave(&lock, flags);
-#else
-	flags = hw_local_irq_save();
-#endif
-	value = __raw_readl(globalregs[type] + reg_offset);
-	value &= ~bits;
-	__raw_writel(value, globalregs[type] + reg_offset);
-#ifndef CONFIG_NKERNEL
-	spin_unlock_irqrestore(&lock, flags);
-#else
-	hw_local_irq_restore(flags);
-#endif
+	__raw_writel(bits, REG_GLB_CLR(globalregs[type] + reg_offset));
 }
 
 EXPORT_SYMBOL(sprd_greg_read);
