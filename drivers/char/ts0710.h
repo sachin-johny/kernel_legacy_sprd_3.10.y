e45baad0856c9d1698477fddf690199067d69cbf
kewang <ke.wang@spreadtrum.com>
2012-10-19 11:05:30 +0800

    Bug#83656 Flow control in mux need to be fixed(fix last commit)
    [bug number  ]
    [root cause  ]
    [changes     ]1.externed mux channel from 15 - 31
                  2.fix post_recv_worker in mux
    [side effects]
    [reviewers   ]
    
    Change-Id: Iae60a746a230a704c17d96b0fc845f511479ffcb

--- drivers/char/ts0710.h
+++ drivers/char/ts0710.h
@@ -67,7 +67,7 @@
 #include <asm/byteorder.h>
 #include <asm/types.h>
 
-#define TS0710_MAX_CHN 17
+#define TS0710_MAX_CHN	33
 
 #define SET_PF(ctr) ((ctr) | (1 << 4))
 #define CLR_PF(ctr) ((ctr) & 0xef)
