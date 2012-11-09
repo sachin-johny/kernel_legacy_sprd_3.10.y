601f2d48d88c74d3d7570b4f74f28249ab45e93e
robot <zhulin.lian@spreadtrum.com>
2012-11-09 13:31:12 -0500

    fixup sp8825 poweroff and reboot fail
    Change-Id: If9689f65c5c75d113bc29fcf2892c277016bcded

--- arch/arm/mach-sc8825/pm_debug.c
+++ arch/arm/mach-sc8825/pm_debug.c
@@ -14,7 +14,6 @@
 #include <linux/init.h>
 #include <linux/suspend.h>
 #include <linux/errno.h>
-#include <mach/globalregs.h>
 #include <asm/irqflags.h>
 #include <linux/io.h>
 #include <linux/delay.h>
@@ -27,6 +26,57 @@ void inc_irq(int irq)
 {
 
 }
+
+#include <mach/system.h>
+#include <mach/adi.h>
+#include <mach/regs_ana_glb.h>
+static void sc8825_power_off(void)
+{
+	/*ture off all modules ldo*/
+	sci_adi_raw_write(ANA_REG_GLB_LDO_PD_CTRL1,	0x5555);
+	sci_adi_raw_write(ANA_REG_GLB_LDO_PD_CTRL0,	0x5555);
+
+	/*ture off all system cores ldo*/
+	sci_adi_clr(ANA_REG_GLB_LDO_PD_RST, 0x3ff);
+	sci_adi_set(ANA_REG_GLB_LDO_PD_SET, 0x3ff);
+}
+
+static void sc8825_machine_restart(char mode, const char *cmd)
+{
+	/* Flush the console to make sure all the relevant messages make it
+	 * out to the console drivers */
+	mdelay(500);
+
+	/* Disable interrupts first */
+	local_irq_disable();
+	local_fiq_disable();
+
+	/*
+	  * FIXME: Do not turn off cache before ldrex/strex!
+	  */
+
+	/*
+	 * Now call the architecture specific reboot code.
+	 */
+	arch_reset(mode, cmd);
+
+	/*
+	 * Whoops - the architecture was unable to reboot.
+	 * Tell the user!
+	 */
+	mdelay(1000);
+	printk("Reboot failed -- System halted\n");
+	while (1);
+}
+
+static int __init sc8825_power_init(void)
+{
+	pm_power_off = sc8825_power_off;
+	arm_pm_restart = sc8825_machine_restart;
+	pr_info("power off %p, restart %p\n", pm_power_off, arm_pm_restart);
+	return 0;
+}
+arch_initcall(sc8825_power_init);
 
 #include <linux/regulator/consumer.h>
 int __init sc8825_ldo_slp_init(void)
