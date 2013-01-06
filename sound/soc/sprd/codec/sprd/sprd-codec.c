55e90368803eb909e243314e96840350ff08837c
ken.kuang <ken.kuang@spreadtrum.com>
2013-01-06 17:17:21 +0800

    bug#0000 modify inter pa setting value map to nv setting
    [bug number ]
    [root cause ]
    [changes ] inter pa setting adjust.
    [side effects]
    [reviewers ]
    
    Change-Id: I65414afd4dd0d1afb511b6dd195d41e36a12ca5f

--- sound/soc/sprd/codec/sprd/sprd-codec.c
+++ sound/soc/sprd/codec/sprd/sprd-codec.c
@@ -127,13 +127,13 @@ struct sprd_codec_mixer {
 
 struct sprd_codec_inter_pa {
 	/* FIXME little endian */
-	int is_classD_mode:1;
-	int is_LDO_mode:1;
-	int is_DEMI_mode:1;
-	int is_auto_LDO_mode:1;
 	int LDO_V_sel:4;
 	int DTRI_F_sel:4;
-	int RESV:21;
+	int is_DEMI_mode:1;
+	int is_classD_mode:1;
+	int is_LDO_mode:1;
+	int is_auto_LDO_mode:1;
+	int RESV:20;
 };
 
 struct sprd_codec_pa_setting {
