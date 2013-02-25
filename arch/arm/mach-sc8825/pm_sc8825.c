f8ae8dd7fd5bcbbcd93d977d3841df59569c59e7
$ vi .repo/manifests/default.xmlliangyahui <liangyh@spreadst.com>
2013-02-25 18:03:22 +0800

    Bug#000 fix bug at localtimer enabled
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: If8511676b1178537e255ff89c3e8adde90d82483

--- arch/arm/mach-sc8825/pm_sc8825.c
+++ arch/arm/mach-sc8825/pm_sc8825.c
@@ -33,6 +33,7 @@
 #include <mach/irqs.h>
 #include <mach/sci.h>
 #include <mach/emc_repower.h>
+#include <linux/clockchips.h>
 
 extern int sc8825_get_clock_status(void);
 extern void secondary_startup(void);
@@ -1121,9 +1122,21 @@ static void init_gr(void)
 	sci_glb_write(REG_GLB_CLK_EN, val, -1UL );
 }
 
+void enable_cpuidle(int enable)
+{
+#if defined(CONFIG_LOCAL_TIMERS)
+	if(enable)
+		sci_glb_set(REG_AHB_AHB_CTL1, BIT_ARM_AUTO_GATE_EN);
+	else
+		sci_glb_clr(REG_AHB_AHB_CTL1, BIT_ARM_AUTO_GATE_EN);
+#endif
+}
+EXPORT_SYMBOL(enable_cpuidle);
+
 #ifdef CONFIG_NKERNEL
 void sc8825_idle(void)
 {
+	int this_cpu = smp_processor_id();
 	int val;
 	if (!need_resched()) {
 		hw_local_irq_disable();
@@ -1137,7 +1150,14 @@ void sc8825_idle(void)
 				l2x0_suspend();
 				*/
 #endif
+#if defined(CONFIG_LOCAL_TIMERS)
+				clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER, &this_cpu);
+#endif
 				cpu_do_idle();
+#if defined(CONFIG_LOCAL_TIMERS)
+				clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT, &this_cpu);
+#endif
+
 #ifdef CONFIG_CACHE_L2X0
 				/*
 				l2x0_resume(1);
