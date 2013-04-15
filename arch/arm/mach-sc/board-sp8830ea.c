6bdbbd8906bab71b705d6527d8df878768e0ebb3
kui.wang <kui.wang@spreadtrum.com>
2013-04-15 16:46:40 -0400

    resolve conflict file
    Change-Id: I30496bb43d6fe7a750b2baabd3fd1419fd418089

--- arch/arm/mach-sc/board-sp8830ea.c
+++ arch/arm/mach-sc/board-sp8830ea.c
@@ -44,8 +44,15 @@
 
 #include <mach/sci.h>
 #include <mach/hardware.h>
+#if defined(CONFIG_ARCH_SC8825)
 #include <mach/regs_glb.h>
 #include <mach/regs_ahb.h>
+#elif defined(CONFIG_ARCH_SC8830)
+#include <mach/regs_sc8830_aon_apb.h>
+#include <mach/regs_sc8830_ap_apb.h>
+#include <mach/regs_sc8830_aon_clk.h>
+#include <mach/regs_sc8830_ap_ahb.h>
+#endif
 
 #include "devices.h"
 
@@ -551,12 +558,22 @@ int __init sc8825_regulator_init(void)
 
 int __init __clock_init_early(void)
 {
+#if defined(CONFIG_ARCH_SC8825)
 	pr_info("ahb ctl0 %08x, ctl2 %08x glb gen0 %08x gen1 %08x clk_en %08x\n",
 		sci_glb_raw_read(REG_AHB_AHB_CTL0),
 		sci_glb_raw_read(REG_AHB_AHB_CTL2),
 		sci_glb_raw_read(REG_GLB_GEN0),
 		sci_glb_raw_read(REG_GLB_GEN1),
 		sci_glb_raw_read(REG_GLB_CLK_EN));
+#elif defined(CONFIG_ARCH_SC8830)
+	pr_info("ahb ctl0 %08x, ctl2 %08x glb aon apb0 %08x aon apb1 %08x clk_en %08x\n",
+		sci_glb_raw_read(REG_AP_AHB_AHB_EB),
+		sci_glb_raw_read(REG_AP_AHB_AHB_RST),
+		sci_glb_raw_read(REG_AON_APB_APB_EB0),
+		sci_glb_raw_read(REG_AON_APB_APB_EB1),
+		sci_glb_raw_read(REG_AON_CLK_PUB_AHB_CFG));
+#endif
+#if defined(CONFIG_ARCH_SC8825)
 	/* FIXME: Force disable all unused clocks */
 	sci_glb_clr(REG_AHB_AHB_CTL0,
 		BIT_AXIBUSMON2_EB	|
@@ -634,6 +651,47 @@ int __init __clock_init_early(void)
 		BIT_SPI2_EB    		|
 		BIT_UART3_EB   		|
 		0);
+#elif defined(CONFIG_ARCH_SC8830)
+	sci_glb_clr(REG_AP_AHB_AHB_EB,
+		BIT_BUSMON2_EB		|
+		BIT_BUSMON1_EB		|
+		BIT_BUSMON0_EB		|
+		BIT_SPINLOCK_EB		|
+		BIT_GPS_EB		|
+		BIT_EMMC_EB		|
+		BIT_SDIO2_EB		|
+		BIT_SDIO1_EB		|
+		BIT_SDIO0_EB		|
+		BIT_DRM_EB		|
+		BIT_NFC_EB		|
+		BIT_DMA_EB		|
+		BIT_USB_EB		|
+		BIT_GSP_EB		|
+		BIT_DISPC1_EB		|
+		BIT_DISPC0_EB		|
+		BIT_DSI_EB		|
+		0);
+	sci_glb_clr(REG_AP_APB_APB_EB,
+		BIT_INTC3_EB		|
+		BIT_INTC2_EB		|
+		BIT_INTC1_EB		|
+		BIT_IIS1_EB		|
+		BIT_UART2_EB		|
+		BIT_UART0_EB		|
+		BIT_SPI1_EB		|
+		BIT_SPI0_EB		|
+		BIT_IIS0_EB		|
+		BIT_I2C0_EB		|
+		BIT_SPI2_EB		|
+		BIT_UART3_EB		|
+		0);
+	sci_glb_clr(REG_AON_APB_APB_RTC_EB,
+		BIT_KPD_RTC_EB		|
+		BIT_KPD_EB		|
+		BIT_EFUSE_EB		|
+		0);
+#endif
+#if defined(CONFIG_ARCH_SC8825)
 	sci_glb_clr(REG_AHB_CA5_CFG,
 //		BIT_CA5_CLK_DBG_EN	|
 		0);
@@ -664,7 +722,18 @@ int __init __clock_init_early(void)
 	//		BIT_TDPLL_DIV4OUT_FORCE_PD	|	/* clk_192m */
 	//		BIT_TDPLL_DIV5OUT_FORCE_PD	|	/* clk_153p6m */
 			0);
-
+#elif defined(CONFIG_ARCH_SC8830)
+	sci_glb_clr(REG_AON_APB_APB_EB0,
+		BIT_AUDIF_EB			|
+		BIT_VBC_EB			|
+		BIT_PWM3_EB			|
+		BIT_PWM1_EB			|
+		0);
+	sci_glb_clr(REG_AON_APB_APB_EB1,
+		BIT_AUX1_EB			|
+		BIT_AUX0_EB			|
+		0);
+#endif
 	printk("sc clock module early init ok\n");
 	return 0;
 }
--- arch/arm/mach-sc/board-sp8830fpga.c
+++ arch/arm/mach-sc/board-sp8830fpga.c
@@ -46,12 +46,13 @@
 #include <mach/hardware.h>
 #if defined(CONFIG_ARCH_SC8825)
 #include <mach/regs_glb.h>
+#include <mach/regs_ahb.h>
 #elif defined(CONFIG_ARCH_SC8830)
 #include <mach/regs_sc8830_aon_apb.h>
 #include <mach/regs_sc8830_ap_apb.h>
 #include <mach/regs_sc8830_aon_clk.h>
+#include <mach/regs_sc8830_ap_ahb.h>
 #endif
-#include <mach/regs_ahb.h>
 
 #include "devices.h"
 
@@ -566,12 +567,13 @@ int __init __clock_init_early(void)
 		sci_glb_raw_read(REG_GLB_CLK_EN));
 #elif defined(CONFIG_ARCH_SC8830)
 	pr_info("ahb ctl0 %08x, ctl2 %0x8 glb aon apb0 %08x aon apb1 %08x clk_en %08x\n",
-		sci_glb_raw_read(REG_AHB_AHB_CTL0),
-		sci_glb_raw_read(REG_AHB_AHB_CTL2),
+		sci_glb_raw_read(REG_AP_AHB_AHB_EB),
+		sci_glb_raw_read(REG_AP_AHB_AHB_RST),
 		sci_glb_raw_read(REG_AON_APB_APB_EB0),
 		sci_glb_raw_read(REG_AON_APB_APB_EB1),
 		sci_glb_raw_read(REG_AON_CLK_PUB_AHB_CFG));
 #endif
+#if defined(CONFIG_ARCH_SC8825)
 	/* FIXME: Force disable all unused clocks */
 	sci_glb_clr(REG_AHB_AHB_CTL0,
 		BIT_AXIBUSMON2_EB	|
@@ -617,7 +619,7 @@ int __init __clock_init_early(void)
 //		BIT_CLK_ULPI_EN		|
 //		BIT_CLK_USB_REF_EN	|
 		0);
-#if defined(CONFIG_ARCH_SC8825)
+
 	sci_glb_clr(REG_GLB_GEN0,
 		BIT_IC3_EB          |
 		BIT_IC2_EB          |
@@ -651,6 +653,25 @@ int __init __clock_init_early(void)
 		BIT_UART3_EB   		|
 		0);
 #elif defined(CONFIG_ARCH_SC8830)
+	sci_glb_clr(REG_AP_AHB_AHB_EB,
+		BIT_BUSMON2_EB		|
+		BIT_BUSMON1_EB		|
+		BIT_BUSMON0_EB		|
+		BIT_SPINLOCK_EB		|
+		BIT_GPS_EB		|
+		BIT_EMMC_EB		|
+		BIT_SDIO2_EB		|
+		BIT_SDIO1_EB		|
+		BIT_SDIO0_EB		|
+		BIT_DRM_EB		|
+		BIT_NFC_EB		|
+		BIT_DMA_EB		|
+		BIT_USB_EB		|
+		BIT_GSP_EB		|
+		BIT_DISPC1_EB		|
+		BIT_DISPC0_EB		|
+		BIT_DSI_EB		|
+		0);
 	sci_glb_clr(REG_AP_APB_APB_EB,
 		BIT_INTC3_EB		|
 		BIT_INTC2_EB		|
@@ -671,10 +692,10 @@ int __init __clock_init_early(void)
 		BIT_EFUSE_EB		|
 		0);
 #endif
+#if defined(CONFIG_ARCH_SC8825)
 	sci_glb_clr(REG_AHB_CA5_CFG,
 //		BIT_CA5_CLK_DBG_EN	|
 		0);
-#if defined(CONFIG_ARCH_SC8825)
 	sci_glb_clr(REG_GLB_GEN1,
 		BIT_AUDIF_AUTO_EN	|
 		BIT_VBC_EN			|
