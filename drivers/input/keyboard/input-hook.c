3cdf4cdaaaba786247c71f89162972e2132df011
luther <luther.ge@spreadtrum.com>
2013-04-22 17:52:07 +0800

    bug#155353 power key interrupt always report wrong button status after deep sleep
    [root cause  ] power key interrupt report wrong button value
    [changes     ] filter these wrong status
    [side effects]
    [reviewers   ]
    
    Change-Id: Iddd1c0296d999cf25da31a618f8e4aebc5979848

--- drivers/input/keyboard/input-hook.c
+++ drivers/input/keyboard/input-hook.c
@@ -125,9 +125,15 @@ void input_report_key_hook(struct input_dev *dev, unsigned int code, int value)
 			.private = NULL, /* like dev */
 			.trigger_watch_event_cb = trigger_watch_powerkey,
 		};
-		if (value)
-			trigger_watch_event_start(&twe, 6 * 1000);
-		else
-			trigger_watch_event_stop(&twe);
+		static int pre_value = -1;
+		if (value != pre_value) {
+			if (value)
+				trigger_watch_event_start(&twe, 6 * 1000);
+			else
+				trigger_watch_event_stop(&twe);
+		} else {
+			pr_warn("Key %d%c is dithering.", code, value ? 'D' : 'U');
+		}
+		pre_value = value;
 	}
 }
