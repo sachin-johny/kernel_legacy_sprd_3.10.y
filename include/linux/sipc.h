feac0f4b52cc4bbfb6113d9fc1e2182d2cc846d3
peter.huang <peter.huang@spreadtrum.com>
2013-05-14 15:40:06 +0800

    Bug#164853 saudio porting to shark
    Change-Id: I1c6c602b035f15bd3270e5ade1545d36d3eec696

--- include/linux/sipc.h
+++ include/linux/sipc.h
@@ -48,9 +48,12 @@ enum {
 	SMSG_CH_DATA0,		/* 2G/3G wirleless data */
 	SMSG_CH_DATA1,		/* 2G/3G wirleless data */
 	SMSG_CH_DATA2,		/* 2G/3G wirleless data */
-	SMSG_CH_VBC,		/* audio conrol channel */
-	SMSG_CH_PLAYBACK, 	/* audio playback channel */
-	SMSG_CH_CAPTURE,	/* audio capture channel */
+	SMSG_CH_VBC_T,		/* audio conrol channel for td */
+	SMSG_CH_PLAYBACK_T, 	/* audio playback channel for td */
+	SMSG_CH_CAPTURE_T,	/* audio capture channel for td */
+	SMSG_CH_VBC_W,		/* audio conrol channel for w */
+	SMSG_CH_PLAYBACK_W, 	/* audio playback channel for w */
+	SMSG_CH_CAPTURE_W,	/* audio capture channel for w */
 	SMSG_CH_NR,		/* total channel number */
 };
 
