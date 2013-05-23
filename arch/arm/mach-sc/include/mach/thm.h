a4d062e6b505708f0ba5e294417dec167243cc58
mingwei.zhang <mingwei.zhang@spreadtrum.com>
2013-05-23 15:11:04 +0800

    add thm drv
    Change-Id: I7d67880d4e223287cc2d3f93a62a72db6f6e8afc

---
+++ arch/arm/mach-sc/include/mach/thm.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2013 Spreadtrum Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ ************************************************
+ * Automatically generated C config: don't edit *
+ ************************************************
+ */
+
+#ifndef __CTL_THM_H__
+#define __CTL_THM_H__
+
+#define SPRD_ARM_SENSOR  0
+#define SPRD_PMIC_SENSOR    1
+extern int sprd_thm_temp_read(u32 sensor);
+
+#endif
