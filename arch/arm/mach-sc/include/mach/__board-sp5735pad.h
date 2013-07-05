e034746793386af39bca751d6a7efc77e03a14e9
wei.ren <wei.ren@spreadtrum.com>
2013-07-05 18:59:46 +0800

    Bug#186404 shark PAD 配置更新
    Change-Id: If93316b0a987a9888a7042f1d8ac82dc9a103cd1

--- arch/arm/mach-sc/include/mach/__board-sp5735pad.h
+++ arch/arm/mach-sc/include/mach/__board-sp5735pad.h
@@ -21,9 +21,16 @@
 #define GPIO_TOUCH_RESET         81
 #define GPIO_TOUCH_IRQ           82
 
-#define GPIO_SENSOR_RESET        41
-#define GPIO_MAIN_SENSOR_PWN     42
-#define GPIO_SUB_SENSOR_PWN      43
+#define GPIO_SENSOR_RESET        186//41
+#define GPIO_MAIN_SENSOR_PWN     187//42
+#define GPIO_SUB_SENSOR_PWN      188//43
+
+#define SPRD_FLASH_OFST          0x890
+#define SPRD_FLASH_CTRL_BIT      0x8000
+#define SPRD_FLASH_LOW_VAL       0x3
+#define SPRD_FLASH_HIGH_VAL      0xF
+#define SPRD_FLASH_LOW_CUR       110
+#define SPRD_FLASH_HIGH_CUR      470
 
 #define USB_OTG_CABLE_DETECT     72
 
@@ -36,9 +43,12 @@
 #define EIC_VCHG_OVI            (A_EIC_START + 6)
 #define EIC_VBAT_OVI            (A_EIC_START + 7)
 
+#define HEADSET_DETECT_GPIO_ACTIVE_LOW 0
+
 #define EIC_KEY_POWER           (EIC_POWER_PBINT)
 #define HEADSET_BUTTON_GPIO		(EIC_AUD_HEAD_BUTTON)
-#define HEADSET_DETECT_GPIO		139
+#define HEADSET_DETECT_GPIO		(EIC_AUD_HEAD_INST2)
+#define HEADSET_SWITCH_GPIO	237
 
 #define SPI0_CMMB_CS_GPIO        156
 #define SPI1_WIFI_CS_GPIO        44
@@ -57,5 +67,8 @@
 #define GPIO_WIFI_SHUTDOWN	230
 #define GPIO_WIFI_IRQ		234
 
-#define GPIO_PLSENSOR_IRQ	213
+#define GPIO_PROX_INT 162
+#define GPIO_GYRO_INT1 163
+#define GPIO_M_RSTN	161
+#define GPIO_M_DRDY 164
 #endif
