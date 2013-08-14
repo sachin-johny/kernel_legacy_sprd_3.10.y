c12616a29427ddeb8b4e06a358e0682f53f509b5
Jack Jiang <jack.jiang@spreadtrum.com>
2013-08-14 14:41:15 +0800

    Bug#202121 update backlight driver for pwm mode
    	[bug number ] 202121
    	[root cause ]
    	[changes ] the backlight driver can support whiteled mode
    	and pwm mode
    	[side effect ] none
    	[reviewers ] yong.li
    
    Change-Id: I5163d1e6f1f2744b6e96e7bce10b13d6f4a4e3b5
    Signed-off-by: Jack Jiang <jack.jiang@spreadtrum.com>

--- arch/arm/mach-sc8810/devices.c
+++ arch/arm/mach-sc8810/devices.c
@@ -188,9 +188,22 @@ struct platform_device sprd_otg_device = {
 	.resource	= sprd_otg_resource,
 };
 
+/*if the backlight is driven by pwm, please config the pwm info
+ *if the backlight is driven by PWMB, the pwm index is 1 as following
+ */
+struct resource sprd_bl_resource[] = {
+	[0] = {
+		.start	= 1,
+		.end	= 1,
+		.flags	= IORESOURCE_IO,
+	},
+};
+
 struct platform_device sprd_backlight_device = {
 	.name           = "sprd_backlight",
 	.id             =  -1,
+	.num_resources	= ARRAY_SIZE(sprd_bl_resource),
+	.resource	= sprd_bl_resource,
 };
 
 static struct resource sprd_i2c_resources0[] = {
