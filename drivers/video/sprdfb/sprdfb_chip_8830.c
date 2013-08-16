2d7684e0cbe93601181e38d44fb55831dec5a331
congfu.zhao <congfu.zhao@spreadtrum.com>
2013-08-16 13:36:59 -0400

    Bug#202968 clean fbdriver log
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: Iadbb4ac42e35e9186ca5f967a810c2c6867fb5f7

--- drivers/video/sprdfb/sprdfb_chip_8830.c
+++ drivers/video/sprdfb/sprdfb_chip_8830.c
@@ -22,12 +22,6 @@ void dsi_enable(void)
 	sci_glb_set(REG_AP_AHB_MISC_CKG_EN, BIT_DPHY_REF_CKG_EN);
 	sci_glb_set(REG_AP_AHB_MISC_CKG_EN, BIT_DPHY_CFG_CKG_EN);
 	sci_glb_set(DSI_REG_EB, DSI_BIT_EB);
-	
-	printk("REG_AP_AHB_MISC_CKG_EN:%x \n",dispc_glb_read(REG_AP_AHB_MISC_CKG_EN));
-	printk("REG_AP_AHB_MISC_CKG_EN:%x \n",REG_AP_AHB_MISC_CKG_EN);
-	printk("DSI_REG_EB:%x \n",dispc_glb_read(DSI_REG_EB));
-	printk("DSI_REG_EB:%x \n",DSI_REG_EB);
-	printk("BIT_DPHY_REF_CKG_EN:%lx,BIT_DPHY_CFG_CKG_EN:%lx \n",BIT_DPHY_REF_CKG_EN,BIT_DPHY_CFG_CKG_EN);
 }
 
 void dsi_disable(void)
@@ -40,12 +34,15 @@ void dsi_disable(void)
 
 void dispc_print_clk(void)
 {
-	printk("sprdfb:0x402e0004 = 0x%x\n", dispc_glb_read((SPRD_AONAPB_BASE+0x4)));
-	printk("sprdfb:0x20d00000 = 0x%x\n", dispc_glb_read(SPRD_AHB_BASE));
-	printk("sprdfb:0x71300000 = 0x%x\n", dispc_glb_read(SPRD_APBREG_BASE));
-	printk("sprdfb:0x71200034 = 0x%x\n", dispc_glb_read((SPRD_APBCKG_BASE+0x34)));
-	printk("sprdfb:0x71200030 = 0x%x\n", dispc_glb_read((SPRD_APBCKG_BASE+0x30)));
-	printk("sprdfb:0x7120002c = 0x%x\n", dispc_glb_read((SPRD_APBCKG_BASE+0x2c)));
+    uint32_t reg_val0,reg_val1,reg_val2;
+    reg_val0 = dispc_glb_read(SPRD_AONAPB_BASE+0x4);
+    reg_val1 = dispc_glb_read(SPRD_AHB_BASE);
+    reg_val2 = dispc_glb_read(SPRD_APBREG_BASE);
+    printk("sprdfb:0x402e0004 = 0x%x 0x20d00000 = 0x%x 0x71300000 = 0x%x \n",reg_val0, reg_val1, reg_val2);
+    reg_val0 = dispc_glb_read(SPRD_APBCKG_BASE+0x34);
+    reg_val1 = dispc_glb_read(SPRD_APBCKG_BASE+0x30);
+    reg_val2 = dispc_glb_read(SPRD_APBCKG_BASE+0x2c);
+    printk("sprdfb:0x71200034 = 0x%x 0x71200030 = 0x%x 0x7120002c = 0x%x \n",reg_val0, reg_val1, reg_val2);
 }
 
 
