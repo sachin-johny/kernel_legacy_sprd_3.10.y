2e24e0c97c9491337b781ccc8b6fb6e4e01f357c
dewu.jiang <dewu.jiang@spreadtrum.com>
2013-08-17 16:44:57 +0800

    Bug	#203664 Add shark sipc support cp2 .
    Change-Id: Id8fff1bc0a2fcaca6e87e6c41698b17f27febe20

--- arch/arm/mach-sc/devices.h
+++ arch/arm/mach-sc/devices.h
@@ -102,6 +102,12 @@ extern struct platform_device sprd_seth2_wcdma_device;
 extern struct platform_device sprd_saudio_wcdma_device;
 extern struct platform_device sprd_saudio_voip_device;
 #endif
+#ifdef CONFIG_SIPC_WCN
+extern struct platform_device sprd_spipe_wcn_device;
+extern struct platform_device sprd_slog_wcn_device;
+extern struct platform_device sprd_cproc_wcn_device;
+extern struct platform_device sprd_sttybt_td_device;
+#endif
 
 extern struct platform_device sprd_a7_pmu_device;
 
