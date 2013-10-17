d1e9b52f3585ba6bccf0260d727ced7075c1bde6
Jianping.Wang <jianping.wang@spreadtrum.com>
2013-10-17 16:14:07 +0800

    bug#227231    add debug information for DCAM timeout。
    [bug number ] bug 227231
    [root cause ]
    [changes ]
                   add debug information for DCAM timeout。
    
    [side effects] none
    [reviewers ]   baggio.he
    
    Change-Id: I49e04fdcb427cfc41242eb6df518d4ab1b959541

--- drivers/media/video/sprd_dcam/sc8830/dcam_v4l2.c
+++ drivers/media/video/sprd_dcam/sc8830/dcam_v4l2.c
@@ -2338,7 +2338,7 @@ LOCAL void sprd_timer_callback(unsigned long data)
 
 	if (0 == atomic_read(&dev->run_flag)) {
 		printk("DCAM timeout.\n");
-		//sprd_v4l2_print_reg();
+		sprd_v4l2_print_reg();
 		node.irq_flag = V4L2_TIMEOUT;
 		node.f_type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 		ret = sprd_v4l2_queue_write(&dev->queue, &node);
