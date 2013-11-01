c925fad54bfdea2333c8ab8641aad46270991544
xi.zhang <xi.zhang@spreadtrum.com>
2013-11-01 17:56:15 +0800

    BUG #230289 [bug number  ]bug #230289 [root cause  ]update for modify the ION size of the 7730ec [changes     ]update for modify the ION size of the 7730ec [side effects]NO [reviewers   ]Baggio.He
    Change-Id: I8f1689957313a8bf32cb266d2f2846838e7cd093

--- arch/arm/mach-sc/include/mach/__board-sp7730ec.h
+++ arch/arm/mach-sc/include/mach/__board-sp7730ec.h
@@ -71,4 +71,9 @@
 #define GPIO_GYRO_INT1 163
 #define GPIO_M_RSTN	161
 #define GPIO_M_DRDY 164
+
+/* General Info for ION*/
+#define SPRD_ION_SIZE            (52 * SZ_1M)
+#define SPRD_ION_OVERLAY_SIZE    (8 * SZ_1M)
+
 #endif
