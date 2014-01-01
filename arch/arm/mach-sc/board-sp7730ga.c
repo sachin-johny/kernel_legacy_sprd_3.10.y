6ef8793e7ca563ed7680aa14da2fc746b03dc310
Giya Li(李佳) <giya.li@spreadtrum.com>
2014-01-01 13:01:37 +0800

    Bug #262700
    [bug number  ] 262700
    [root cause  ] new feature
    [changes     ] remove emmc device for 7730ga
    [side effects] n/a
    [reviewers   ]
    
    Change-Id: I1e22a9eb274d36a05727063210e7fb5237ca8f2e

--- arch/arm/mach-sc/board-sp7730ga.c
+++ arch/arm/mach-sc/board-sp7730ga.c
@@ -127,7 +127,7 @@ static struct platform_device *devices[] __initdata = {
 	&sprd_spi2_device,
 	&sprd_keypad_device,
 	&sprd_battery_device,
-	&sprd_emmc_device,
+	//&sprd_emmc_device,
 	&sprd_sdio0_device,
 	&sprd_sdio1_device,
 	/* &sprd_sdio2_device, */
