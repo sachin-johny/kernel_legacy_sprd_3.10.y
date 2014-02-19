fb8a1be2798595c9615b02cbc902fe3bad06ab41
yuhua.shi <yuhua.shi@spreadtrum.com>
2014-02-19 12:38:15 +0800

    Revert "Bug #280094 thm drv for dolphin"
    This reverts commit 3ef17819fce48b1fdfb4837f41ad62a7dfdc050a

--- drivers/power/sprd_2713_power.c
+++ drivers/power/sprd_2713_power.c
@@ -45,12 +45,10 @@
 #define SPRDBAT_DEBUG(format, arg...)
 #endif
 #if defined(CONFIG_ARCH_SCX15)
-#if 0 // xiangzhao.chen deleted for sprd_thm.c   2014.01.28
 int sprd_thm_temp_read(u32 sensor)
 {
 	return 0;
 }
-#endif 
 #endif
 
 #define SPRDBAT_CV_TRIGGER_CURRENT		1/2
