ff90f52fb1de586fb99a65c3839980c550c62574
ying.xu <ying.xu@spreadtrum.com>
2014-03-28 10:17:27 +0800

    Bug#294596  change execute sequence of kheader
    [self test   ] build pass
    
    Change-Id: Ic6d5ce48723f06139acb24fe8d7114995880282a

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -15,14 +15,12 @@ $(KERNEL_CONFIG): kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
 	mkdir -p $(KERNEL_OUT)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- $(KERNEL_DEFCONFIG)
 STRIP := ./prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/arm-eabi/bin/strip
-$(TARGET_PREBUILT_KERNEL) : $(KERNEL_OUT) $(KERNEL_CONFIG)
+kernelheader: $(KERNEL_CONFIG)
+	mkdir -p $(KERNEL_OUT)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install
+$(TARGET_PREBUILT_KERNEL) : kernelheader
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- zImage -j4
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
 	@-mkdir -p $(KERNEL_MODULES_OUT)
 	@-find $(KERNEL_OUT) -name *.ko | xargs -I{} cp {} $(KERNEL_MODULES_OUT)
 	@-find $(KERNEL_MODULES_OUT) -name *.ko | xargs -I{} $(STRIP) -d {}
-
-kernelheader:
-	mkdir -p $(KERNEL_OUT)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install
