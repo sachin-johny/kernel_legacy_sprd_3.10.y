7366caff38a8c9a74f768be5f758b7443f078399
leon.li <leon.li@spreadtrum.com>
2014-04-23 10:56:13 +0800

    Bug #303212,just use astyle to format the soucre code.Nothing anymore.
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    [self test   ]OK
    
    Change-Id: Icb0b4b8e48cfd3253deb29df203d7a765b045264

--- drivers/misc/sprd_vsp/sprd_vsp_sc8830.c
+++ drivers/misc/sprd_vsp/sprd_vsp_sc8830.c
@@ -571,10 +571,10 @@ static int vsp_release (struct inode *inode, struct file *filp)
     kfree(filp->private_data);
     filp->private_data=NULL;
 
-    
-#ifndef CONFIG_OF 
+
+#ifndef CONFIG_OF
     clk_disable(vsp_hw_dev.mm_clk);
-#endif 
+#endif
     printk(KERN_INFO "VSP mmi_clk close!!");
 
     return 0;
