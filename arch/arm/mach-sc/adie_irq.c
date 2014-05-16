fe6bd05bcf6034561c955bb3ae8b2423df430505
wei.qiao <wei.qiao@spreadtrum.com>
2014-05-16 10:52:08 +0800

    Bug #306183 correct adie irq mask/unmake fun and sub irq process
    [bug number  ] 306183
    [root cause  ] when using dts, adie irq mask/unmake is not suitable
    [changes     ] correct adie irq mask/unmake fun and sub irq process
    [side effects] no
    [self test   ] test pass on 8830gea
    [reviewers   ] mark.yang;zhizhou.zhang;geng.ren
    
    Change-Id: I6c9f566ba91e4a14f5301aa28dee8ced42b4f2d2

--- arch/arm/mach-sc/adie_irq.c
+++ arch/arm/mach-sc/adie_irq.c
@@ -18,6 +18,7 @@
 #include <linux/irq.h>
 #include <linux/io.h>
 #include <linux/interrupt.h>
+#include <linux/slab.h>
 #ifdef CONFIG_OF
 #include <linux/irqdomain.h>
 #include <linux/of_address.h>
@@ -51,6 +52,14 @@
 #error "pls define interrupt number mask value"
 #endif
 
+/* used to define the start irq in xlate fun */
+#define ADIE_SUB_IRQ_START 32
+
+/* used to save vitual irq num */
+static int *virq_table = NULL;
+
+/* the total irq num of adie domain */
+static int irqnums = 0;
 
 static struct irq_domain *irq_domain;
 void sprd_ack_ana_irq(struct irq_data *data)
@@ -60,14 +69,42 @@ void sprd_ack_ana_irq(struct irq_data *data)
 
 static void sprd_mask_ana_irq(struct irq_data *data)
 {
-	int offset = data->irq - IRQ_ANA_INT_START;
+	int offset;
+#ifdef CONFIG_OF
+	int i;
+
+	for (i = 0; i < irqnums; i++) {
+		if (virq_table[i] == data->irq) {
+			offset = i;
+			break;
+		}
+	}
+	if (i == irqnums)
+		return;
+#else
+	offset = data->irq - IRQ_ANA_INT_START;
+#endif
 	pr_debug("%s %d\n", __FUNCTION__, data->irq);
 	sci_adi_write(ANA_REG_INT_EN, 0, BIT(offset) & MASK_ANA_INT);
 }
 
 static void sprd_unmask_ana_irq(struct irq_data *data)
 {
-	int offset = data->irq - IRQ_ANA_INT_START;
+	int offset;
+#ifdef CONFIG_OF
+	int i;
+
+	for (i = 0; i < irqnums; i++) {
+		if (virq_table[i] == data->irq) {
+			offset = i;
+			break;
+		}
+	}
+	if (i == irqnums)
+		return;
+#else
+	offset = data->irq - IRQ_ANA_INT_START;
+#endif
 	pr_debug("%s %d\n", __FUNCTION__, data->irq);
 	sci_adi_write(ANA_REG_INT_EN, BIT(offset) & MASK_ANA_INT, 0);
 }
@@ -92,7 +129,7 @@ static irqreturn_t sprd_muxed_ana_handler(int irq, void *dev_id)
 #ifndef CONFIG_OF
 		irq_ana = IRQ_ANA_INT_START + i;
 #else
-		irq_ana = irq_find_mapping(irq_domain, i);
+		irq_ana = virq_table[i];
 #endif
 		pr_debug("%s generic_handle_irq %d\n", __FUNCTION__, irq_ana);
 		generic_handle_irq(irq_ana);
@@ -124,10 +161,38 @@ void __init ana_init_irq(void)
 	static const struct of_device_id irqchip_of_match_##name	\
 	__used __section(__irqchip_of_table)				\
 	= { .compatible = compstr, .data = fn }
+
+static int adie_irq_domain_xlate(struct irq_domain *d,
+				struct device_node *ctrlr,
+				const u32 *intspec, unsigned int intsize,
+				unsigned long *out_hwirq, unsigned int *out_type)
+{
+	if (WARN_ON(intsize < 1))
+		return -EINVAL;
+	*out_hwirq = intspec[0] + ADIE_SUB_IRQ_START;
+	*out_type = (intsize > 1) ? intspec[1] : IRQ_TYPE_NONE;
+	return 0;
+}
+
+static int adie_irq_domain_map(struct irq_domain *h, unsigned int virq,
+							irq_hw_number_t hw)
+{
+	virq_table[hw-ADIE_SUB_IRQ_START] = virq;
+	irq_set_chip_and_handler(virq, &sprd_muxed_ana_chip,
+			handle_level_irq);
+	set_irq_flags(virq, IRQF_VALID);
+
+	return 0;
+}
+
+const struct irq_domain_ops adie_irq_domain_ops = {
+	.xlate = adie_irq_domain_xlate,
+	.map = adie_irq_domain_map,
+};
 int __init adi_of_init(struct device_node *node, struct device_node *parent)
 {
 	void __iomem *v_base;
-	uint32_t reg_size, irqnums, i;
+	uint32_t reg_size;
 	struct resource res;
 	int irq;
 
@@ -138,7 +203,7 @@ int __init adi_of_init(struct device_node *node, struct device_node *parent)
 		pr_err("no reg of property specified for adi\n");
 		return -ENODEV;
 	}
-	v_base = res.start;
+	v_base = (void __iomem *)res.start;
 	reg_size = resource_size(&res);
 
 	if (of_property_read_u32(node, "sprd,irqnums", &irqnums)){
@@ -146,20 +211,18 @@ int __init adi_of_init(struct device_node *node, struct device_node *parent)
 		BUG();
 	}
 
-	irq_domain = irq_domain_add_linear(node, irqnums,
-			&irq_domain_simple_ops, NULL);
+	irq_domain = irq_domain_add_linear(node, irqnums + ADIE_SUB_IRQ_START,
+			&adie_irq_domain_ops, NULL);
 	BUG_ON(!irq_domain);
+
+	virq_table = kmalloc(sizeof(int)*irqnums, GFP_KERNEL);
+	BUG_ON(!virq_table);
 
 	if (parent) {
 		irq = irq_of_parse_and_map(node, 0);
 		setup_irq(irq, &__adie_mux_irq);
 	}
-	for(i = 0; i < irqnums; i++){
-		irq = irq_create_mapping(irq_domain, i);
-		irq_set_chip_and_handler(irq, &sprd_muxed_ana_chip,
-					 handle_level_irq);
-		set_irq_flags(irq, IRQF_VALID);
-	}
+
 	return 0;
 }
 IRQCHIP_DECLARE(sprd_adi, "sprd,adi-bus", adi_of_init);
