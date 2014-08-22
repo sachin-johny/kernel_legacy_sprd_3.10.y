1a71c5c79de00be02f4442d59c19c7a3a8df3481
yuchuan.sun <yuchuan.sun@spreadtrum.com>
2014-08-22 14:17:12 +0800

    Bug #338352 [sprdroid4.4_3.10][CTP]porting from sprdroid4.4_3.10_sharkl_re to sprdroid4.4_3.10
    [bug number  ] 338352
    [root cause  ] porting
    [changes     ]
    [side effects]
    [self test   ] <sprdroid4.4_3.10_sharkl_re><scx35l_sp9630_dt><TP is ok>
    [whether AOB ]
    [reviewers   ]
    
    Change-Id: I1ca65a4b570b3f7c7c133156c9c906316bbd54fc

--- drivers/input/touchscreen/focaltech/focaltech.c
+++ drivers/input/touchscreen/focaltech/focaltech.c
@@ -79,7 +79,7 @@
 
 #define	TOUCH_VIRTUAL_KEYS
 #define	MULTI_PROTOCOL_TYPE_B	1
-#define	TS_MAX_FINGER		5
+#define	TS_MAX_FINGER		2
 
 #define	FTS_PACKET_LENGTH	128
 
@@ -491,13 +491,9 @@ static int ft5x0x_update_data(void)
 		#if MULTI_PROTOCOL_TYPE_B
 			input_mt_slot(data->input_dev, buf[6*i+5]>>4);
 			input_mt_report_slot_state(data->input_dev, MT_TOOL_FINGER, true);
-		#else
-			input_report_abs(data->input_dev, ABS_MT_TRACKING_ID, buf[6*i+5]>>4);
 		#endif
 			input_report_abs(data->input_dev, ABS_MT_POSITION_X, x);
 			input_report_abs(data->input_dev, ABS_MT_POSITION_Y, y);
-			input_report_abs(data->input_dev, ABS_MT_PRESSURE, ft_pressure);
-			input_report_abs(data->input_dev, ABS_MT_TOUCH_MAJOR, ft_size);
 			input_report_key(data->input_dev, BTN_TOUCH, 1);
 		#if !MULTI_PROTOCOL_TYPE_B
 			input_mt_sync(data->input_dev);
@@ -852,12 +848,8 @@ static int ft5x0x_ts_probe(struct i2c_client *client, const struct i2c_device_id
 #endif
 	input_set_abs_params(input_dev,ABS_MT_POSITION_X, 0, pdata->TP_MAX_X, 0, 0);
 	input_set_abs_params(input_dev,ABS_MT_POSITION_Y, 0, pdata->TP_MAX_Y, 0, 0);
-	input_set_abs_params(input_dev,ABS_MT_TOUCH_MAJOR, 0, 15, 0, 0);
-	input_set_abs_params(input_dev,ABS_MT_WIDTH_MAJOR, 0, 15, 0, 0);
-	input_set_abs_params(input_dev,ABS_MT_PRESSURE, 0, 127, 0, 0);
-#if !MULTI_PROTOCOL_TYPE_B
-	input_set_abs_params(input_dev,ABS_MT_TRACKING_ID, 0, 255, 0, 0);
-#endif
+	input_set_abs_params(input_dev,ABS_MT_TOUCH_MAJOR, 0, 255, 0, 0);
+	input_set_abs_params(input_dev,ABS_MT_WIDTH_MAJOR, 0, 255, 0, 0);
 	#if 0
 	/*ft5306's firmware is qhd, ft5316's firmware is 720p*/
 	if (uc_reg_value == 0x0a || uc_reg_value == 0x0) {
