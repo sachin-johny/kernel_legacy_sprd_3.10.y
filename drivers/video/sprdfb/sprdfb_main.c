34edb19e2726e1ff2c7af126cdac38d75fae2f77
jason.lv <jason.lv@spreadtrum.com>
2014-10-14 19:12:34 +0800

    Bug #361392, Change the framebuffer information pixel format
    [bug number  ] 361392
    [root cause  ] framebuffer pixel offset informat is not right
    [changes     ] change the RGB offsets
    [side effects] NA
    [self test   ] pass on sharkl test
    [whether AOB ] Not
    [reviewers   ] andrew.wu
    
    Change-Id: If15cc675e38842820afe4d879b93ae7d0e03d32e

--- drivers/video/sprdfb/sprdfb_main.c
+++ drivers/video/sprdfb/sprdfb_main.c
@@ -237,13 +237,13 @@ static void setup_fb_info(struct sprdfb_device *dev)
 
 	/* only support two pixel format */
 	if (dev->bpp == 32) { /* ABGR */
-		fb->var.red.offset     = 24;
+		fb->var.red.offset     = 16;
 		fb->var.red.length     = 8;
 		fb->var.red.msb_right  = 0;
-		fb->var.green.offset   = 16;
+		fb->var.green.offset   = 8;
 		fb->var.green.length   = 8;
 		fb->var.green.msb_right = 0;
-		fb->var.blue.offset    = 8;
+		fb->var.blue.offset    = 0;
 		fb->var.blue.length    = 8;
 		fb->var.blue.msb_right = 0;
 	} else { /*BGR*/
