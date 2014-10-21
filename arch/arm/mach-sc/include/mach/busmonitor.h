917b69bf4c50b0e84ca1099f0cb9f880e9623817
eric.long <eric.long@spreadtrum.com>
2014-10-21 19:25:06 +0800

    Bug #359766
    [bug number  ] 359766
    [root cause  ] bus monitor code cause cali fail
    [changes     ] bus monitor code
    [side effects] no
    [self test   ] yes
    [whether AOB ]
    [reviewers   ] baolin.wan
    
    Change-Id: I5147940e652024445770108f21d0414e68a3d0aa

--- arch/arm/mach-sc/include/mach/busmonitor.h
+++ arch/arm/mach-sc/include/mach/busmonitor.h
@@ -1,75 +1,25 @@
 #ifndef __SPRD_BM_H__
 #define __SPRD_BM_H__
 
-#define AXI_BM_INTC_REG				0x0
-#define AXI_BM_CFG_REG				0x4
-#define AXI_BM_ADDR_MIN_REG			0x8
-#define AXI_BM_ADDR_MAX_REG			0xc
-#define AXI_BM_ADDR_MSK_REG			0x10
-#define AXI_BM_DATA_MIN_L_REG		0x14
-#define AXI_BM_DATA_MIN_H_REG		0x18
-#define AXI_BM_DATA_MAX_L_REG		0x1c
-#define AXI_BM_DATA_MAX_H_REG		0x20
-#define AXI_BM_DATA_MSK_L_REG		0x24
-#define AXI_BM_DATA_MSK_H_REG		0x28
-#define AXI_BM_CNT_WIN_LEN_REG		0x2c
-#define AXI_BM_PEAK_WIN_LEN_REG		0x30
-#define AXI_BM_MATCH_ADDR_REG		0x34
-#define AXI_BM_MATCH_CMD_REG		0x38
-#define AXI_BM_MATCH_DATA_L_REG		0x3c
-#define AXI_BM_MATCH_DATA_H_REG		0x40
-#define AXI_BM_RTRANS_IN_WIN_REG	0x44
-#define AXI_BM_RBW_IN_WIN_REG		0x48
-#define AXI_BM_RLATENCY_IN_WIN_REG	0x4c
-#define AXI_BM_WTRANS_IN_WIN_REG	0x50
-#define AXI_BM_WBW_IN_WIN_REG		0x54
-#define AXI_BM_WLATENCY_IN_WIN_REG	0x58
-#define AXI_BM_PEAKBW_IN_WIN_REG	0x5c
-
-#define SPRD_AHB_BM_NAME "sprd_ahb_bm"
-#define SPRD_AXI_BM_NAME "sprd_axi_bm"
-
-#define SPRD_BM_SUCCESS	0
-#define BM_INT_MSK_STS	BIT(31)
-#define BM_INT_CLR		BIT(29)
-#define BM_INT_EN		BIT(28)
-#define BM_CNT_CLR		BIT(4)
-#define BM_CNT_START	BIT(3)
-#define BM_CNT_EN		BIT(1)
-#define BM_CHN_EN		BIT(0)
-#define BM_DEBUG_ALL_CHANNEL	0xFF
+#define SPRD_AHB_BM_NAME "sprd_ahb_busmonitor"
+#define SPRD_AXI_BM_NAME "sprd_axi_busmonitor"
 
 /*depending on the platform*/
 enum sci_bm_index {
-	AXI_BM0_CA7 = 0x0,
-	AXI_BM1_DISP,
-	AXI_BM2_GSP_GPU,
-	AXI_BM3_ZIP_AP,
-	AXI_BM4_MM,
-	AXI_BM5_CP0ARM_WCDMA,
-	AXI_BM6_CP0_DSP,
-	AXI_BM7_HARQ_LTEACC,
-	AXI_BM8_CP1_DPS,
-	AXI_BM9_CP1_A5,
-	AHB_BM0_DAP_A7_DMA,
-	AHB_BM1_SDIO_EMMC,
-	AHB_BM2_NFC_USB,
+	AXI_BM0 = 0x0,
+	AXI_BM1,
+	AXI_BM2,
+	AXI_BM3,
+	AXI_BM4,
+	AXI_BM5,
+	AXI_BM6,
+	AXI_BM7,
+	AXI_BM8,
+	AXI_BM9,
+	AHB_BM0,
+	AHB_BM1,
+	AHB_BM2,
 	BM_SIZE,
-};
-
-enum sci_ahb_bm_index {
-	AHB_BM0_DAP = AHB_BM0_DAP_A7_DMA,
-	AHB_BM0_CA7,
-	AHB_BM0_DMA_WR,
-	AHB_BM0_DMA_RD,
-	AHB_BM1_SDIO0,
-	AHB_BM1_SDIO1,
-	AHB_BM1_SDIO2,
-	AHB_BM1_EMMC,
-	AHB_BM2_NFC,
-	AHB_BM2_USB,
-	AHB_BM2_HSIC,
-	BM_CHANNEL_SIZE,
 };
 
 enum sci_bm_chn {
@@ -89,58 +39,18 @@ enum sci_bm_mode {
 	*/
 };
 
-struct bm_per_info {
-	u32 t_start;
-	u32 t_stop;
-	u32 tmp1;
-	u32 tmp2;
-	u32 per_data[10][6];
-};
-
 struct sci_bm_cfg {
-	u32 bm_bits;
-	u32 win_len;
 	u32 addr_min;
 	u32 addr_max;
-	u32 addr_msk;
-	u32 data_min_l;
-	u32 data_min_h;
-	u32 data_max_l;
-	u32 data_max_h;
-	u32 data_msk_l;
-	u32 data_msk_h;
-	u32 chn;
-	u32 fun;
-	u32 data;
-	u32 peak_win_len;
+	/*only be effective for AHB busmonitor*/
+	u64 data_min;
+	u64 data_max;
 	enum sci_bm_mode bm_mode;
 };
 
-struct sci_bm_reg {
-	u32 intc;
-	u32 cfg;
-	u32 addr_min;
-	u32 addr_max;
-	u32 addr_msk;
-	u32 data_min_l;
-	u32 data_min_h;
-	u32 data_max_l;
-	u32 data_max_h;
-	u32 data_msk_l;
-	u32 data_msk_h;
-	u32 cnt_win_len;
-	u32 peak_win_len;
-	u32 match_addr;
-	u32 match_cmd;
-	u32 match_data_l;
-	u32 match_data_h;
-	u32 rtrans_in_win;
-	u32 rbw_in_win;
-	u32 rlatency_in_win;
-	u32 wtrans_in_win;
-	u32 wbw_in_win;
-	u32 wlatency_in_win;
-	u32 peakbw_in_win;
-};
+int sci_bm_set_point(enum sci_bm_index bm_index, enum sci_bm_chn chn,
+	const struct sci_bm_cfg *cfg, void(*call_back)(void));
+
+void sci_bm_unset_point(enum sci_bm_index bm_index);
 
 #endif
