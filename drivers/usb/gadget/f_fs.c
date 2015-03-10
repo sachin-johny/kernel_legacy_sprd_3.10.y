18c2a6924aae89d8f8a4d69775f6833240f5ef86
jiayong.yang <jiayong.yang@spreadtrum.com>
2015-03-10 13:22:48 +0800

    Bug #412237
    [bug number  ]412237
    [root cause  ]ffs_epfile_io() refuse signal.
    [changes     ]ffs_epfile_io() can be interruptible.
    [side effects]no side effect
    [self test   ][Sharkl4M] USB functions
    [whether AOB ]
    [reviewers   ]chunhou.wang
    
    Change-Id: Ibc74d806e709863848861def19946e456b6b1c3a

--- drivers/usb/gadget/f_fs.c
+++ drivers/usb/gadget/f_fs.c
@@ -23,7 +23,6 @@
 #include <linux/export.h>
 #include <linux/hid.h>
 #include <asm/unaligned.h>
-
 #include <linux/usb/composite.h>
 #include <linux/usb/functionfs.h>
 
@@ -144,6 +143,8 @@ struct ffs_data {
 	struct usb_request		*ep0req;		/* P: mutex */
 	struct completion		ep0req_completion;	/* P: mutex */
 	int				ep0req_status;		/* P: mutex */
+	struct completion		epin_completion;	/* P: mutex */
+	struct completion		epout_completion;	/* P: mutex */
 
 	/* reference counter */
 	atomic_t			ref;
@@ -749,8 +750,11 @@ static const struct file_operations ffs_ep0_operations = {
 
 static void ffs_epfile_io_complete(struct usb_ep *_ep, struct usb_request *req)
 {
+	struct  ffs_ep *ep = _ep->driver_data;
+
 	ENTER();
-	if (likely(req->context)) {
+	/* req may be freed during unbind */
+	if (ep && ep->req && likely(req->context)) {
 		struct ffs_ep *ep = _ep->driver_data;
 		ep->status = req->status ? req->status : req->actual;
 		complete(req->context);
@@ -761,6 +765,7 @@ static ssize_t ffs_epfile_io(struct file *file,
 			     char __user *buf, size_t len, int read)
 {
 	struct ffs_epfile *epfile = file->private_data;
+	struct ffs_data	*ffs = epfile->ffs;
 	struct ffs_ep *ep;
 	char *data = NULL;
 	ssize_t ret, data_len;
@@ -844,22 +849,30 @@ first_try:
 		ret = -EBADMSG;
 	} else {
 		/* Fire the request */
-		DECLARE_COMPLETION_ONSTACK(done);
+		struct completion	*done;
 
 		struct usb_request *req = ep->req;
-		req->context  = &done;
 		req->complete = ffs_epfile_io_complete;
 		req->buf      = data;
 		req->length   = data_len;
 
+		if (read) {
+			INIT_COMPLETION(ffs->epout_completion);
+			req->context = done = &ffs->epout_completion;
+		} else {
+			INIT_COMPLETION(ffs->epin_completion);
+			req->context = done = &ffs->epin_completion;
+		}
 		ret = usb_ep_queue(ep->ep, req, GFP_ATOMIC);
 
 		spin_unlock_irq(&epfile->ffs->eps_lock);
 
 		if (unlikely(ret < 0)) {
 			/* nop */
-		} else  {
-			wait_for_completion(&done);
+		} else if (unlikely(wait_for_completion_interruptible(done))) {
+			ret = -EINTR;
+			usb_ep_dequeue(ep->ep, req);
+		} else {
 			ret = ep->status;
 			if (read && ret > 0){
 				ret = min_t(size_t, ret, len);
@@ -1375,6 +1388,8 @@ static struct ffs_data *ffs_data_new(void)
 	spin_lock_init(&ffs->eps_lock);
 	init_waitqueue_head(&ffs->ev.waitq);
 	init_completion(&ffs->ep0req_completion);
+	init_completion(&ffs->epin_completion);
+	init_completion(&ffs->epout_completion);
 
 	/* XXX REVISIT need to update it in some places, or do we? */
 	ffs->ev.can_stall = 1;
