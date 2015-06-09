34e5231776caca8fdea8d53a850ed714f3378673
xu wentao <xuwt@spreadst.com>
2015-06-09 09:51:59 +0800

    Bug #444301 add cid csd ext-csd file node into mmc's debugfs, that catch cid/csd/ext-csd info to usr space
    [bug number  ] 444301
    [root cause  ] lava emmc/sd test need to show cid/csd/ext-csd info
    [changes     ] add cid/csd/ext-csd file node into /mmc/core/debugfs.c
    [side effects]
    [self test   ] <7731gea><follow bug steps, test 1 times, same as expected results ><test case PF3, pass>
    [whether AOB ]
    [reviewers   ]
    
    Change-Id: Id771eebbf3729bcd9d2314522a017436e3827b83

--- drivers/mmc/core/debugfs.c
+++ drivers/mmc/core/debugfs.c
@@ -273,6 +273,8 @@ DEFINE_SIMPLE_ATTRIBUTE(mmc_dbg_card_status_fops, mmc_dbg_card_status_get,
 
 #define EXT_CSD_STR_LEN 1025
 
+
+
 static int mmc_ext_csd_open(struct inode *inode, struct file *filp)
 {
 	struct mmc_card *card = inode->i_private;
@@ -334,6 +336,169 @@ static const struct file_operations mmc_dbg_ext_csd_fops = {
 	.llseek		= default_llseek,
 };
 
+
+static int mmc_extcsd_show(struct seq_file *s, void *data)
+{
+	struct mmc_card		*card = s->private;
+	struct mmc_ext_csd	ext_csd = card->ext_csd;	/* mmc v4 extended card specific */
+
+	seq_printf(s,"\nExtended Card specific Info\n\n");
+	seq_printf(s, "rev :\t\t%x \n", ext_csd.rev);
+	seq_printf(s, "erase_group_def :\t\t%x \n", ext_csd.erase_group_def);
+	seq_printf(s, "sec_feature_support :\t\t%d \n", ext_csd.sec_feature_support);
+	seq_printf(s, "rel_sectors :\t\t%d \n", ext_csd.rel_sectors);
+	seq_printf(s, "rel_param :\t\t%d \n", ext_csd.rel_param);
+	seq_printf(s, "part_config :\t\t%d \n", ext_csd.part_config);
+	seq_printf(s, "cache_ctrl :\t\t%d \n", ext_csd.cache_ctrl);
+	seq_printf(s, "rst_n_function :\t\t%d \n", ext_csd.rst_n_function);
+	seq_printf(s, "max_packed_writes :\t\t%d \n", ext_csd.max_packed_writes);
+	seq_printf(s, "max_packed_reads :\t\t%d \n", ext_csd.max_packed_reads);
+	seq_printf(s, "packed_event_en :\t\t%d \n", ext_csd.packed_event_en);
+	seq_printf(s, "part_time :\t\t%d \n", ext_csd.part_time);
+	seq_printf(s, "sa_timeout :\t\t%d \n", ext_csd.sa_timeout);
+	seq_printf(s, "generic_cmd6_time :\t\t%d \n", ext_csd.generic_cmd6_time);
+	seq_printf(s, "power_off_longtime :\t\t%d \n", ext_csd.power_off_longtime);
+	seq_printf(s, "power_off_notification :\t\t%d \n", ext_csd.power_off_notification);
+	seq_printf(s, "hs_max_dtr :\t\t%d \n", ext_csd.hs_max_dtr);
+	seq_printf(s, "sectors :\t\t%d \n", ext_csd.sectors);
+	seq_printf(s, "card_type :\t\t%d \n", ext_csd.card_type);
+	seq_printf(s, "hc_erase_size :\t\t%d \n", ext_csd.hc_erase_size);
+	seq_printf(s, "hc_erase_timeout :\t\t%d \n", ext_csd.hc_erase_timeout);
+	seq_printf(s, "sec_trim_mult :\t\t%d \n", ext_csd.sec_trim_mult);
+	seq_printf(s, "sec_erase_mult :\t\t%d \n", ext_csd.sec_erase_mult);
+	seq_printf(s, "trim_timeout :\t\t%d \n", ext_csd.trim_timeout);
+	seq_printf(s, "enhanced_area_en :\t\t%d \n", ext_csd.enhanced_area_en);
+	seq_printf(s, "enhanced_area_offset :\t\t%lld \n", ext_csd.enhanced_area_offset);
+	seq_printf(s, "enhanced_area_size :\t\t%d \n", ext_csd.enhanced_area_size);
+	seq_printf(s, "cache_size :\t\t%d \n", ext_csd.cache_size);
+	seq_printf(s, "hpi_en :\t\t%d \n", ext_csd.hpi_en);
+	seq_printf(s, "hpi :\t\t%d \n", ext_csd.hpi);
+	seq_printf(s, "hpi_cmd :\t\t%d \n", ext_csd.hpi_cmd);
+	seq_printf(s, "bkops :\t\t%d \n", ext_csd.bkops);
+	seq_printf(s, "bkops_en :\t\t%d \n", ext_csd.bkops_en);
+	seq_printf(s, "data_sector_size :\t\t%d \n", ext_csd.data_sector_size);
+	seq_printf(s, "data_tag_unit_size :\t\t%d \n", ext_csd.data_tag_unit_size);
+	seq_printf(s, "boot_ro_lock :\t\t%d \n", ext_csd.boot_ro_lock);
+	seq_printf(s, "boot_ro_lockable :\t\t%d \n", ext_csd.boot_ro_lockable);
+	seq_printf(s, "raw_exception_status :\t\t%d \n", ext_csd.raw_exception_status);
+	seq_printf(s, "raw_partition_support :\t\t%d \n", ext_csd.raw_partition_support);
+	seq_printf(s, "raw_rpmb_size_mult :\t\t%d \n", ext_csd.raw_rpmb_size_mult);
+	seq_printf(s, "raw_erased_mem_count :\t\t%d \n", ext_csd.raw_erased_mem_count);
+	seq_printf(s, "raw_ext_csd_structure :\t\t%d \n", ext_csd.raw_ext_csd_structure);
+	seq_printf(s, "raw_card_type :\t\t%d \n", ext_csd.raw_card_type);
+	seq_printf(s, "out_of_int_time :\t\t%d \n", ext_csd.out_of_int_time);
+	seq_printf(s, "raw_s_a_timeout :\t\t%d \n", ext_csd.raw_s_a_timeout);
+	seq_printf(s, "raw_erase_timeout_mult :\t\t%d \n", ext_csd.raw_erase_timeout_mult);
+	seq_printf(s, "raw_hc_erase_grp_size :\t\t%d \n", ext_csd.raw_hc_erase_grp_size);
+	seq_printf(s, "raw_sec_trim_mult :\t\t%d \n", ext_csd.raw_sec_trim_mult);
+	seq_printf(s, "raw_sec_erase_mult :\t\t%d \n", ext_csd.raw_sec_erase_mult);
+	seq_printf(s, "raw_sec_feature_support :\t\t%d \n", ext_csd.raw_sec_feature_support);
+	seq_printf(s, "raw_trim_mult :\t\t%d \n", ext_csd.raw_trim_mult);
+	seq_printf(s, "raw_bkops_status :\t\t%d \n", ext_csd.raw_bkops_status);
+	seq_printf(s, "raw_sectors[0] :\t\t%d \n", ext_csd.raw_sectors[0]);
+	seq_printf(s, "raw_sectors[1] :\t\t%d \n", ext_csd.raw_sectors[1]);
+	seq_printf(s, "raw_sectors[2] :\t\t%d \n", ext_csd.raw_sectors[2]);
+	seq_printf(s, "raw_sectors[3] :\t\t%d \n", ext_csd.raw_sectors[3]);
+	seq_printf(s, "feature_support:\t\t%d \n", ext_csd.feature_support);
+
+	return 0;
+}
+
+
+
+static int mmc_cid_show(struct seq_file *s, void *data)
+{
+	struct mmc_card		*card = s->private;
+	struct mmc_cid		cid = card->cid;		/* card identification */
+
+	seq_printf(s, "\nCard identification  Info:\n\n");
+	seq_printf(s,"manfid :\t%d \n", cid.manfid);
+	seq_printf(s,"prod_name :\t%s \n", cid.prod_name);
+	seq_printf(s,"prv :\t\t0x%x \n", cid.prv);
+	seq_printf(s,"serial :\t%d \n", cid.serial);
+	seq_printf(s,"oemid :\t\t%d \n", cid.oemid);
+	seq_printf(s,"year :\t\t%d \n", cid.year);
+	seq_printf(s,"hwrev :\t\t0x%x \n", cid.hwrev);
+	seq_printf(s,"fwrev :\t\t0x%x \n", cid.fwrev); 
+	seq_printf(s,"month :\t\t0x%x \n", cid.month);
+
+
+	return 0;
+}
+
+
+
+static int mmc_csd_show(struct seq_file *s, void *data)
+{
+	struct mmc_card		*card = s->private;
+	struct mmc_csd		csd = card->csd;		/* card specific */
+
+
+    seq_printf(s, "\nCard specific Info\n\n");
+
+	seq_printf(s, "structure :\t\t%x \n", csd.structure);
+	seq_printf(s, "mmca_vsn :\t\t%x \n", csd.mmca_vsn);
+	seq_printf(s, "cmdclass :\t\t%d \n", csd.cmdclass);
+	seq_printf(s, "tacc_clks :\t\t%d \n", csd.tacc_clks);
+	seq_printf(s, "tacc_ns :\t\t%d \n", csd.tacc_ns);
+	seq_printf(s, "c_size :\t\t%d \n", csd.c_size);
+	seq_printf(s, "r2w_factor :\t\t%d \n", csd.r2w_factor);
+	seq_printf(s, "max_dtr :\t\t%d\n", csd.max_dtr); 
+	seq_printf(s, "erase_size :\t\t%d \n", csd.erase_size);
+	seq_printf(s, "read_blkbits :\t\t%d \n", csd.read_blkbits);
+	seq_printf(s, "write_blkbits :\t\t%d \n", csd.write_blkbits);
+	seq_printf(s, "capacity :\t\t%d \n", csd.capacity);
+	seq_printf(s, "read_partial :\t\t%x \n", csd.read_partial);
+	seq_printf(s, "read_misalign :\t\t%x \n", csd.read_misalign);
+	seq_printf(s, "write_partial :\t\t%x \n", csd.write_partial);
+	seq_printf(s, "write_misalign :\t%x \n", csd.write_misalign);
+
+
+	return 0;
+}
+
+
+
+
+static int mmc_cid_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mmc_cid_show, inode->i_private);
+}
+
+static const struct file_operations mmc_dbg_cid_fops = {
+	.open		= mmc_cid_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+
+static int mmc_csd_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mmc_csd_show, inode->i_private);
+}
+
+static const struct file_operations mmc_dbg_csd_fops = {
+	.open		= mmc_csd_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int mmc_extcsd_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mmc_extcsd_show, inode->i_private);
+}
+
+static const struct file_operations mmc_dbg_extcsd_fops = {
+	.open		= mmc_extcsd_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+
+
 void mmc_add_card_debugfs(struct mmc_card *card)
 {
 	struct mmc_host	*host = card->host;
@@ -365,6 +530,20 @@ void mmc_add_card_debugfs(struct mmc_card *card)
 		if (!debugfs_create_file("ext_csd", S_IRUSR, root, card,
 					&mmc_dbg_ext_csd_fops))
 			goto err;
+
+	if (mmc_card_mmc(card)|| mmc_card_sd(card))
+	if (!debugfs_create_file("cid", S_IRUSR, root, card,
+				&mmc_dbg_cid_fops))
+		goto err;
+
+	if (mmc_card_mmc(card)|| mmc_card_sd(card))
+	if (!debugfs_create_file("csd", S_IRUSR, root, card,
+				&mmc_dbg_csd_fops))
+		goto err;
+	if (mmc_card_mmc(card)|| mmc_card_sd(card))
+	if (!debugfs_create_file("extcsd", S_IRUSR, root, card,
+				&mmc_dbg_extcsd_fops))
+		goto err;
 
 	return;
 
